e19724e6a505f681d2a3ab478d020bb6831849dd
beastzx18 <vs423502@gmail.com>
2021-09-11 22:11:00 +0530

    creating welcome sql

--- tronx/database/__init__.py
+++ tronx/database/__init__.py
@@ -2,3 +2,4 @@ from tronx.database.postgres.afk_sql import *
 from tronx.database.postgres.notes_sql import *
 from tronx.database.postgres.pmpermit_sql import *
 from tronx.database.postgres.dv_sql import *
+from tronx.database.postgres.welcome_sql import *
---
+++ tronx/database/postgres/welcome_sql.py
@@ -0,0 +1,79 @@
+import threading
+
+from sys import platform
+
+from sqlalchemy import (
+	Column, 
+	String, 
+	Integer
+)
+
+from . import SESSION, BASE
+
+
+
+
+# save user ids in whitelists
+class welcome(BASE):
+	__tablename__ = "welcome"
+	
+	keys = Column(String, primary_key=True)
+	values = Column(String)
+	
+	def __init__(self, keys, values):
+		self.keys = keys
+		self.values = values
+
+
+
+
+welcome.__table__.create(checkfirst=True)
+
+INSERTION_LOCK = threading.RLock()
+
+
+
+
+# set, del, get keys & values
+def set_welcome(keys, values):
+	with INSERTION_LOCK:
+		mydata = SESSION.query(welcome).get(keys)
+		try:
+			if not mydata:
+				mydata = welcome(keys, values)
+			else:
+				mydata.values = values
+			SESSION.merge(mydata)
+			SESSION.commit()
+		finally:
+			SESSION.close()
+	return keys
+
+
+
+
+def del_welcome(keys):
+	with INSERTION_LOCK:
+		mydata = SESSION.query(welcome).get(keys)
+		try:
+			if mydata:
+				SESSION.delete(mydata)
+				SESSION.commit()
+		finally:
+			SESSION.close()
+		return False
+
+
+
+
+def get_welcome(keys):
+	mydata = SESSION.query(welcome).get(keys)
+	rep = ""
+	if mydata:
+		rep = str(mydata.values)
+	SESSION.close()
+	return rep
+
+
+
+
