addf0b40ce7d6aac4e8fd4560fd85434ab5f0248
beastzx18 <vs423502@gmail.com>
2021-09-30 23:41:34 +0530

    update

--- tronx/database/__init__.py
+++ tronx/database/__init__.py
@@ -4,3 +4,4 @@ from tronx.database.postgres.pmpermit_sql import *
 from tronx.database.postgres.dv_sql import *
 from tronx.database.postgres.welcome_sql import *
 from tronx.database.postgres.filters_sql import *
+from tronx.datanase.postgres.sudo_sql import *
