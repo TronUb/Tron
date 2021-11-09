81b8ac9a0ee9480c3aad0d1968da2ed59c66c696
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2021-11-09 19:56:07 +0530

    Update animepic.py

--- tronx/modules/animepic.py
+++ tronx/modules/animepic.py
@@ -12,6 +12,19 @@ from tronx.helpers import (
 
 
 
+CMD_HELP.update(
+	{"animepic": (
+		"animepic",
+		{
+		"neko" : "Get a anime neko girl image",
+		}
+		)
+	}
+)
+
+
+
+
 def get_anime_gif(arg):
 	data = requests.get(f"https://nekos.best/api/v1/{arg}").text
 	img = json.loads(data)["url"], 
@@ -27,6 +40,7 @@ def get_anime_gif(arg):
 @app.on_message(gen("neko"))
 async def baka_gif(_, m):
 	try:
+		await m.delete()
 		data = requests.get("https://nekos.best/api/v1/nekos").text
 		data = json.loads(data)
 		await app.send_photo(
@@ -43,6 +57,7 @@ async def baka_gif(_, m):
 @app.on_message(gen("bakagif"))
 async def baka_gif(_, m):
 	try:
+		await m.delete()
 		data = get_anime_gif("baka")
 		await app.send_video(
 			m.chat.id,
@@ -57,8 +72,9 @@ async def baka_gif(_, m):
 
 
 @app.on_message(gen("bitegif"))
-async def baka_gif(_, m):
+async def bite_gif(_, m):
 	try:
+		await m.delete()
 		data = get_anime_gif("bite")
 		await app.send_video(
 			m.chat.id,
@@ -73,8 +89,9 @@ async def baka_gif(_, m):
 
 
 @app.on_message(gen("blushgif"))
-async def baka_gif(_, m):
+async def blush_gif(_, m):
 	try:
+		await m.delete()
 		data = get_anime_gif("blush")
 		await app.send_video(
 			m.chat.id,
@@ -89,8 +106,9 @@ async def baka_gif(_, m):
 
 
 @app.on_message(gen("boredgif"))
-async def baka_gif(_, m):
+async def bored_gif(_, m):
 	try:
+		await m.delete()
 		data = get_anime_gif("bored")
 		await app.send_video(
 			m.chat.id,
@@ -105,8 +123,9 @@ async def baka_gif(_, m):
 
 
 @app.on_message(gen("crygif"))
-async def baka_gif(_, m):
+async def cry_gif(_, m):
 	try:
+		await m.delete()
 		data = get_anime_gif("cry")
 		await app.send_video(
 			m.chat.id,
@@ -121,8 +140,9 @@ async def baka_gif(_, m):
 
 
 @app.on_message(gen("cuddlegif"))
-async def baka_gif(_, m):
+async def cuddle_gif(_, m):
 	try:
+		await m.delete()
 		data = get_anime_gif("cuddle")
 		await app.send_video(
 			m.chat.id,
@@ -137,8 +157,9 @@ async def baka_gif(_, m):
 
 
 @app.on_message(gen("dancegif"))
-async def baka_gif(_, m):
+async def dance_gif(_, m):
 	try:
+		await m.delete()
 		data = get_anime_gif("dance")
 		await app.send_video(
 			m.chat.id,
@@ -153,8 +174,9 @@ async def baka_gif(_, m):
 
 
 @app.on_message(gen("facepalmgif"))
-async def baka_gif(_, m):
+async def facepalm_gif(_, m):
 	try:
+		await m.delete()
 		data = get_anime_gif("facepalm")
 		await app.send_video(
 			m.chat.id,
@@ -169,8 +191,9 @@ async def baka_gif(_, m):
 
 
 @app.on_message(gen("feedgif"))
-async def baka_gif(_, m):
+async def feed_gif(_, m):
 	try:
+		await m.delete()
 		data = get_anime_gif("feed")
 		await app.send_video(
 			m.chat.id,
@@ -185,8 +208,9 @@ async def baka_gif(_, m):
 
 
 @app.on_message(gen("happygif"))
-async def baka_gif(_, m):
+async def happy_gif(_, m):
 	try:
+		await m.delete()
 		data = get_anime_gif("happy")
 		await app.send_video(
 			m.chat.id,
@@ -201,8 +225,9 @@ async def baka_gif(_, m):
 
 
 @app.on_message(gen("highfivegif"))
-async def baka_gif(_, m):
+async def highfive_gif(_, m):
 	try:
+		await m.delete()
 		data = get_anime_gif("highfive")
 		await app.send_video(
 			m.chat.id,
@@ -217,8 +242,9 @@ async def baka_gif(_, m):
 
 
 @app.on_message(gen("huggif"))
-async def baka_gif(_, m):
+async def hug_gif(_, m):
 	try:
+		await m.delete()
 		data = get_anime_gif("hug")
 		await app.send_video(
 			m.chat.id,
@@ -233,8 +259,9 @@ async def baka_gif(_, m):
 
 
 @app.on_message(gen("kissgif"))
-async def baka_gif(_, m):
+async def kiss_gif(_, m):
 	try:
+		await m.delete()
 		data = get_anime_gif("kiss")
 		await app.send_video(
 			m.chat.id,
@@ -249,8 +276,9 @@ async def baka_gif(_, m):
 
 
 @app.on_message(gen("laughgif"))
-async def baka_gif(_, m):
+async def laugh_gif(_, m):
 	try:
+		await m.delete()
 		data = get_anime_gif("laugh")
 		await app.send_video(
 			m.chat.id,
@@ -265,8 +293,9 @@ async def baka_gif(_, m):
 
 
 @app.on_message(gen("patgif"))
-async def baka_gif(_, m):
+async def pat_gif(_, m):
 	try:
+		await m.delete()
 		data = get_anime_gif("pat")
 		await app.send_video(
 			m.chat.id,
@@ -281,8 +310,9 @@ async def baka_gif(_, m):
 
 
 @app.on_message(gen("pokegif"))
-async def baka_gif(_, m):
+async def poke_gif(_, m):
 	try:
+		await m.delete()
 		data = get_anime_gif("poke")
 		await app.send_video(
 			m.chat.id,
@@ -297,8 +327,9 @@ async def baka_gif(_, m):
 
 
 @app.on_message(gen("poutgif"))
-async def baka_gif(_, m):
+async def pout_gif(_, m):
 	try:
+		await m.delete()
 		data = get_anime_gif("pout")
 		await app.send_video(
 			m.chat.id,
@@ -313,8 +344,9 @@ async def baka_gif(_, m):
 
 
 @app.on_message(gen("shruggif"))
-async def baka_gif(_, m):
+async def shrug_gif(_, m):
 	try:
+		await m.delete()
 		data = get_anime_gif("shrug")
 		await app.send_video(
 			m.chat.id,
@@ -329,8 +361,9 @@ async def baka_gif(_, m):
 
 
 @app.on_message(gen("sleepgif"))
-async def baka_gif(_, m):
+async def sleep_gif(_, m):
 	try:
+		await m.delete()
 		data = get_anime_gif("sleep")
 		await app.send_video(
 			m.chat.id,
@@ -345,8 +378,9 @@ async def baka_gif(_, m):
 
 
 @app.on_message(gen("slapgif"))
-async def baka_gif(_, m):
+async def slap_gif(_, m):
 	try:
+		await m.delete()
 		data = get_anime_gif("slap")
 		await app.send_video(
 			m.chat.id,
@@ -361,8 +395,9 @@ async def baka_gif(_, m):
 
 
 @app.on_message(gen("smilegif"))
-async def baka_gif(_, m):
+async def smile_gif(_, m):
 	try:
+		await m.delete()
 		data = get_anime_gif("smile")
 		await app.send_video(
 			m.chat.id,
@@ -377,8 +412,9 @@ async def baka_gif(_, m):
 
 
 @app.on_message(gen("smuggif"))
-async def baka_gif(_, m):
+async def smug_gif(_, m):
 	try:
+		await m.delete()
 		data = get_anime_gif("smug")
 		await app.send_video(
 			m.chat.id,
@@ -393,8 +429,9 @@ async def baka_gif(_, m):
 
 
 @app.on_message(gen("staregif"))
-async def baka_gif(_, m):
+async def stare_gif(_, m):
 	try:
+		await m.delete()
 		data = get_anime_gif("stare")
 		await app.send_video(
 			m.chat.id,
@@ -409,8 +446,9 @@ async def baka_gif(_, m):
 
 
 @app.on_message(gen("thinkgif"))
-async def baka_gif(_, m):
+async def think_gif(_, m):
 	try:
+		await m.delete()
 		data = get_anime_gif("think")
 		await app.send_video(
 			m.chat.id,
@@ -425,8 +463,9 @@ async def baka_gif(_, m):
 
 
 @app.on_message(gen("thumbsupgif"))
-async def baka_gif(_, m):
+async def thumbsup_gif(_, m):
 	try:
+		await m.delete()
 		data = get_anime_gif("thumbsup")
 		await app.send_video(
 			m.chat.id,
@@ -441,8 +480,9 @@ async def baka_gif(_, m):
 
 
 @app.on_message(gen("ticklegif"))
-async def baka_gif(_, m):
+async def ticle_gif(_, m):
 	try:
+		await m.delete()
 		data = get_anime_gif("tickle")
 		await app.send_video(
 			m.chat.id,
@@ -457,8 +497,9 @@ async def baka_gif(_, m):
 
 
 @app.on_message(gen("wavegif"))
-async def baka_gif(_, m):
+async def wave_gif(_, m):
 	try:
+		await m.delete()
 		data = get_anime_gif("wave")
 		await app.send_video(
 			m.chat.id,
@@ -473,8 +514,9 @@ async def baka_gif(_, m):
 
 
 @app.on_message(gen("winkgif"))
-async def baka_gif(_, m):
+async def wink_gif(_, m):
 	try:
+		await m.delete()
 		data = get_anime_gif("wink")
 		await app.send_video(
 			m.chat.id,
