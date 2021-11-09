c3c217c912703a8d1c89268f1af2386e0b704feb
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2021-11-09 22:08:21 +0530

    Update animepic.py

--- tronx/modules/animepic.py
+++ tronx/modules/animepic.py
@@ -13,15 +13,24 @@ from tronx.helpers import (
 
 
 
+
+anime_suffix = "`baka`\n`bite`\n`blush`\n`bored`\n`cry`\n`cuddle`\n`dance`\n`facepalm`\n`feed`\n`happy`\n`highfive`\n`hug`\n`kiss`\n`laugh`\n`pat`\n`poke`\n`pout`\n`shrug`\n`slap`\n`sleep`\n`smile`\n`stare`\n`think`\n`thumbsup`\n`tickle`\n`wave`\n`wink`"
+
+
+
+
 CMD_HELP.update(
 	{"animepic": (
 		"animepic",
 		{
-		"neko" : "Get a anime neko girl image",
+		"neko" : "Get a anime neko girl image.,
+		"gif [suffix]" : "Get gif's of different anime expressions, use the command below to get suffix list.",
+		"giflist" : "Get a list of suffix.,
 		}
 		)
 	}
 )
+
 
 
 
@@ -35,6 +44,24 @@ def get_anime_gif(arg):
 	else:
 		return False
 
+
+
+async def send_gif(m: Message, gif_data):
+	try:
+		await app.send_video(
+			m.chat.id,
+			gif_data[0],
+			caption=gif_data[1]
+		)
+	except Exception as e:
+		await error(m, e)
+
+
+
+
+@app.on_message(gen("giflist")
+async def list_of_suffix(_, m):
+	await send_edit(m, anime_suffix)
 
 
 
@@ -55,474 +82,102 @@ async def baka_gif(_, m):
 
 
 
-@app.on_message(gen("bakagif"))
+@app.on_message(gen("gif"))
 async def baka_gif(_, m):
-	try:
-		await m.delete()
-		data = get_anime_gif("baka")
-		await app.send_video(
-			m.chat.id,
-			data[0],
-			caption=data[1]
-			)
-	except Exception as e:
-		await error(m, e)
-
-
-
-
-
-@app.on_message(gen("bitegif"))
-async def bite_gif(_, m):
-	try:
-		await m.delete()
-		data = get_anime_gif("bite")
-		await app.send_video(
-			m.chat.id,
-			data[0],
-			caption=data[1]
-			)
-	except Exception as e:
-		await error(m, e)
-
-
-
-
-
-@app.on_message(gen("blushgif"))
-async def blush_gif(_, m):
-	try:
-		await m.delete()
-		data = get_anime_gif("blush")
-		await app.send_video(
-			m.chat.id,
-			data[0],
-			caption=data[1]
-			)
-	except Exception as e:
-		await error(m, e)
-
-
-
-
-
-@app.on_message(gen("boredgif"))
-async def bored_gif(_, m):
-	try:
-		await m.delete()
-		data = get_anime_gif("bored")
-		await app.send_video(
-			m.chat.id,
-			data[0],
-			caption=data[1]
-			)
-	except Exception as e:
-		await error(m, e)
-
-
-
-
-
-@app.on_message(gen("crygif"))
-async def cry_gif(_, m):
-	try:
-		await m.delete()
-		data = get_anime_gif("cry")
-		await app.send_video(
-			m.chat.id,
-			data[0],
-			caption=data[1]
-			)
-	except Exception as e:
-		await error(m, e)
-
-
-
-
-
-@app.on_message(gen("cuddlegif"))
-async def cuddle_gif(_, m):
-	try:
-		await m.delete()
-		data = get_anime_gif("cuddle")
-		await app.send_video(
-			m.chat.id,
-			data[0],
-			caption=data[1]
-			)
-	except Exception as e:
-		await error(m, e)
-
-
-
-
-
-@app.on_message(gen("dancegif"))
-async def dance_gif(_, m):
-	try:
-		await m.delete()
-		data = get_anime_gif("dance")
-		await app.send_video(
-			m.chat.id,
-			data[0],
-			caption=data[1]
-			)
-	except Exception as e:
-		await error(m, e)
-
-
-
-
-
-@app.on_message(gen("facepalmgif"))
-async def facepalm_gif(_, m):
-	try:
-		await m.delete()
-		data = get_anime_gif("facepalm")
-		await app.send_video(
-			m.chat.id,
-			data[0],
-			caption=data[1]
-			)
-	except Exception as e:
-		await error(m, e)
-
-
-
-
-
-@app.on_message(gen("feedgif"))
-async def feed_gif(_, m):
-	try:
-		await m.delete()
-		data = get_anime_gif("feed")
-		await app.send_video(
-			m.chat.id,
-			data[0],
-			caption=data[1]
-			)
-	except Exception as e:
-		await error(m, e)
-
-
-
-
-
-@app.on_message(gen("happygif"))
-async def happy_gif(_, m):
-	try:
-		await m.delete()
-		data = get_anime_gif("happy")
-		await app.send_video(
-			m.chat.id,
-			data[0],
-			caption=data[1]
-			)
-	except Exception as e:
-		await error(m, e)
-
-
-
-
-
-@app.on_message(gen("highfivegif"))
-async def highfive_gif(_, m):
-	try:
-		await m.delete()
-		data = get_anime_gif("highfive")
-		await app.send_video(
-			m.chat.id,
-			data[0],
-			caption=data[1]
-			)
-	except Exception as e:
-		await error(m, e)
-
-
-
-
-
-@app.on_message(gen("huggif"))
-async def hug_gif(_, m):
-	try:
-		await m.delete()
-		data = get_anime_gif("hug")
-		await app.send_video(
-			m.chat.id,
-			data[0],
-			caption=data[1]
-			)
-	except Exception as e:
-		await error(m, e)
-
-
-
-
-
-@app.on_message(gen("kissgif"))
-async def kiss_gif(_, m):
-	try:
-		await m.delete()
-		data = get_anime_gif("kiss")
-		await app.send_video(
-			m.chat.id,
-			data[0],
-			caption=data[1]
-			)
-	except Exception as e:
-		await error(m, e)
-
-
-
-
-
-@app.on_message(gen("laughgif"))
-async def laugh_gif(_, m):
-	try:
-		await m.delete()
-		data = get_anime_gif("laugh")
-		await app.send_video(
-			m.chat.id,
-			data[0],
-			caption=data[1]
-			)
-	except Exception as e:
-		await error(m, e)
-
-
-
-
-
-@app.on_message(gen("patgif"))
-async def pat_gif(_, m):
-	try:
-		await m.delete()
-		data = get_anime_gif("pat")
-		await app.send_video(
-			m.chat.id,
-			data[0],
-			caption=data[1]
-			)
-	except Exception as e:
-		await error(m, e)
-
-
-
-
-
-@app.on_message(gen("pokegif"))
-async def poke_gif(_, m):
-	try:
-		await m.delete()
-		data = get_anime_gif("poke")
-		await app.send_video(
-			m.chat.id,
-			data[0],
-			caption=data[1]
-			)
-	except Exception as e:
-		await error(m, e)
-
-
-
-
-
-@app.on_message(gen("poutgif"))
-async def pout_gif(_, m):
-	try:
-		await m.delete()
-		data = get_anime_gif("pout")
-		await app.send_video(
-			m.chat.id,
-			data[0],
-			caption=data[1]
-			)
-	except Exception as e:
-		await error(m, e)
-
-
-
-
-
-@app.on_message(gen("shruggif"))
-async def shrug_gif(_, m):
-	try:
-		await m.delete()
-		data = get_anime_gif("shrug")
-		await app.send_video(
-			m.chat.id,
-			data[0],
-			caption=data[1]
-			)
-	except Exception as e:
-		await error(m, e)
-
-
-
-
-
-@app.on_message(gen("sleepgif"))
-async def sleep_gif(_, m):
-	try:
-		await m.delete()
-		data = get_anime_gif("sleep")
-		await app.send_video(
-			m.chat.id,
-			data[0],
-			caption=data[1]
-			)
-	except Exception as e:
-		await error(m, e)
-
-
-
-
-
-@app.on_message(gen("slapgif"))
-async def slap_gif(_, m):
-	try:
-		await m.delete()
-		data = get_anime_gif("slap")
-		await app.send_video(
-			m.chat.id,
-			data[0],
-			caption=data[1]
-			)
-	except Exception as e:
-		await error(m, e)
-
-
-
-
-
-@app.on_message(gen("smilegif"))
-async def smile_gif(_, m):
-	try:
-		await m.delete()
-		data = get_anime_gif("smile")
-		await app.send_video(
-			m.chat.id,
-			data[0],
-			caption=data[1]
-			)
-	except Exception as e:
-		await error(m, e)
-
-
-
-
-
-@app.on_message(gen("smuggif"))
-async def smug_gif(_, m):
-	try:
-		await m.delete()
-		data = get_anime_gif("smug")
-		await app.send_video(
-			m.chat.id,
-			data[0],
-			caption=data[1]
-			)
-	except Exception as e:
-		await error(m, e)
-
-
-
-
-
-@app.on_message(gen("staregif"))
-async def stare_gif(_, m):
-	try:
-		await m.delete()
-		data = get_anime_gif("stare")
-		await app.send_video(
-			m.chat.id,
-			data[0],
-			caption=data[1]
-			)
-	except Exception as e:
-		await error(m, e)
-
-
-
-
-
-@app.on_message(gen("thinkgif"))
-async def think_gif(_, m):
-	try:
-		await m.delete()
-		data = get_anime_gif("think")
-		await app.send_video(
-			m.chat.id,
-			data[0],
-			caption=data[1]
-			)
-	except Exception as e:
-		await error(m, e)
-
-
-
-
-
-@app.on_message(gen("thumbsupgif"))
-async def thumbsup_gif(_, m):
-	try:
-		await m.delete()
-		data = get_anime_gif("thumbsup")
-		await app.send_video(
-			m.chat.id,
-			data[0],
-			caption=data[1]
-			)
-	except Exception as e:
-		await error(m, e)
-
-
-
-
-
-@app.on_message(gen("ticklegif"))
-async def ticle_gif(_, m):
-	try:
-		await m.delete()
-		data = get_anime_gif("tickle")
-		await app.send_video(
-			m.chat.id,
-			data[0],
-			caption=data[1]
-			)
-	except Exception as e:
-		await error(m, e)
-
-
-
-
-
-@app.on_message(gen("wavegif"))
-async def wave_gif(_, m):
-	try:
-		await m.delete()
-		data = get_anime_gif("wave")
-		await app.send_video(
-			m.chat.id,
-			data[0],
-			caption=data[1]
-			)
-	except Exception as e:
-		await error(m, e)
-
-
-
-
-
-@app.on_message(gen("winkgif"))
-async def wink_gif(_, m):
-	try:
-		await m.delete()
-		data = get_anime_gif("wink")
-		await app.send_video(
-			m.chat.id,
-			data[0],
-			caption=data[1]
-			)
-	except Exception as e:
-		await error(m, e)
+	if long(m) > 1:
+		arg = m.command[1]
+		try:
+			await m.delete()
+			if arg == "baka":
+				data = get_anime_gif("baka")
+				await send_gif(m, data)
+			elif arg == "bite":
+				data = get_anime_gif("bite")
+				await send_gif(m, data)
+			elif arg == "blush":
+				data = get_anime_gif("blush")
+				await send_gif(m, data)
+			elif arg == "bored":
+				data = get_anime_gif("bored")
+				await send_gif(m, data)
+			elif arg == "cry":
+				data = get_anime_gif("cry")
+				await send_gif(m, data)
+			elif arg == "cuddle":
+				data = get_anime_gif("cuddle")
+				await send_gif(m, data)
+			elif arg == "dance":
+				data = get_anime_gif("dance")
+				await send_gif(m, data)
+			elif arg == "facepalm":
+				data = get_anime_gif("facepalm")
+				await send_gif(m, data)
+			elif arg ==  "feed":
+				data = get_anime_gif("feed")
+				await send_gif(m, data)
+			elif arg == "happy":
+				data = get_anime_gif("happy")
+				await send_gif(m, data)
+			elif arg == "highfive":
+				data = get_anime_gif("highfive")
+				await send_gif(m, data)
+			elif arg == "hug":
+				data = get_anime_gif("hug")
+				await send_gif(m, data)
+			elif arg == "kiss":
+				data = get_anime_gif("kiss")
+				await send_gif(m, data)
+			elif arg == "laugh":
+				data = get_anime_gif("laugh")
+				await send_gif(m, data)
+			elif arg == "pat":
+				data = get_anime_gif("pat")
+				await send_gif(m, data)
+			elif arg == "poke":
+				data = get_anime_gif("poke")
+				await send_gif(m, data)
+			elif arg == "pout":
+				data = get_anime_gif("pout")
+				await send_gif(m, data)
+			elif arg == "shrug":
+				data = get_anime_gif("shrug")
+				await send_gif(m, data)
+			elif arg == "sleep":
+				data = get_anime_gif("sleep")
+				await send_gif(m, data)
+			elif arg == "slap":
+				data = get_anime_gif("slap")
+				await send_gif(m, data)
+			elif arg == "smile":
+				data = get_anime_gif("smile")
+				await send_gif(m, data)
+			elif arg == "smug":
+				data = get_anime_gif("smug")
+				await send_gif(m, data)
+			elif arg == "stare":
+				data = get_anime_gif("stare")
+				await send_gif(m, data)
+			elif arg == "think":
+				data = get_anime_gif("think")
+				await send_gif(m, data)
+			elif arg == "thumbsup":
+				data = get_anime_gif("thumbsup")
+				await send_gif(m, data)
+			elif arg == "tickle":
+				data = get_anime_gif("tickle")
+				await send_gif(m, data)
+			elif arg == "wave":
+				data = get_anime_gif("wave")
+				await send_gif(m, data)
+			elif arg == "wink":
+				data = get_anime_gif("wink")
+				await send_gif(m, data)
+			elif arg == "think":
+				data = get_anime_gif("think")
+				await send_gif(m, data)
+			else:
+				await send_edit(m, anime_suffix)
+		except Exception as e:
+			await error(m, e)
+	else:
+		await send_edit(m, "Give me a suffix . . .", mono=True, delme=3)
