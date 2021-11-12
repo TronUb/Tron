b359b0c913811bc0d790dcc15a14b86df9a65e3d
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2021-11-12 17:48:28 +0530

    Update bots.py

--- tronx/helpers/bots.py
+++ tronx/helpers/bots.py
@@ -9,49 +9,41 @@ try:
 		BOT_USERNAME
 		)
 except ImportError:
-	BOT_ID = None
-	BOT_NAME = None
+	BOT_ID = Config.BOT_ID if Config.BOT_ID else None
+	BOT_NAME = Config.BOT_NAME if Config.BOT_NAME else None
 	BOT_USERNAME = Config.BOT_USERNAME if Config.BOT_USERNAME else None     
 
+from tronx.database.postgres import dv_sql as dv
 
 
 
+
+# name of bot (1st priority to database variable)
 def botname():
-	if Config.BOT_NAME:
-		botname = Config.BOT_NAME
-	elif BOT_NAME:
-		botname = BOT_NAME
-	else:
-		botname = None
-	return botname
+	var = dv.getdv("BOT_NAME")
+	var_data = var if bool(var) is True else Config.BOT_NAME
+	data = var_data if var_data else BOT_NAME
+	return data if data else None
 
 
+# username of bot
 def botusername():
-	if Config.BOT_USERNAME:
-		botuname = Config.BOT_USERNAME
-	elif BOT_USERNAME:
-		botuname = BOT_USERNAME
-	else:
-		botuname = None
-	return botuname
+	var = dv.getdv("BOT_USERNAME")
+	var_data = var if bool(var) is True else Config.BOT_USERNAME
+	data = var_data if var_data else BOT_USERNAME
+	return data if data else None
 
 
+# mention of bot
 def botmention():
-	bname = botname()
-	bid = botid()
-	if bname and bid:
-		bmention = mention_markdown(bid, bname)
-	else:
-		bmention = None
-	return bmention
+	return mention_markdown(botid(), botname()) if botid() and bname() else None  
 
 
+# id of bot 
 def botid():
-	if Config.BOT_ID:
-		bid = Config.BOT_ID
-	elif BOT_ID:
-		bid = BOT_ID
-	else:
-		bid = None
-	return bid
+	var = dv.getdv("BOT_ID")
+	var_data = var if bool(var) is True else Config.BOT_ID
+	data = var_data if var_data else BOT_ID
+	return data if data else None
+
 
