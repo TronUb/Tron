904ed354e02ab98aea6a649c6e74c69b529a51c7
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2021-11-18 23:40:30 +0530

    Update help.py

--- tronx/plugins/help.py
+++ tronx/plugins/help.py
@@ -10,10 +10,7 @@ from pyrogram.types import Message
 
 @bot.on_message(filters.command("help"))
 async def send_help(_, m: Message):
-	if m.from_user:
-		await bot.send_message(
-			m.chat.id,
-			"Just use the /start command . . . 🤭"
-			)
-	else:
-		return
+	await bot.send_message(
+		m.chat.id,
+		"Just use the /start command . . . 🤭"
+		)
