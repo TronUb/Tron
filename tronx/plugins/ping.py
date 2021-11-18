4e017f6fe9086c924b1c6c92e3cd071f7a8b63fb
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2021-11-18 23:11:11 +0530

    Update ping.py

--- tronx/plugins/ping.py
+++ tronx/plugins/ping.py
@@ -11,14 +11,24 @@ from tronx import (
 
 
 @bot.on_message(filters.command(["ping"]))
-async def bot_ping(_, m):
-	start = datetime.now()
-	end = datetime.now()
-	ms = (end - start).microseconds / 1000
-	await bot.send_message(
-		m.chat.id,
-		f"Pong !\n`{ms}`\nUptime: `{uptime()}`",
-	)
+async def bot_ping(_, m: Message):
+	if m.chat.type == "bot":
+		start = datetime.now()
+		end = datetime.now()
+		ms = (end - start).microseconds / 1000
+		await bot.send_message(
+			m.chat.id,
+			f"Pong !\n`{ms}`\nUptime: `{uptime()}`"
+		)
+	elif m.chat.type in ["supergroup", "group"]:
+		start = datetime.now()
+		msg = await bot.send_message(
+			m.chat.id,
+			"Pinging . . ."
+		)
+		end = datetime.now()
+		ms = (end - start).microseconds / 1000
+		await msg.edit(f"Pong !\n`{ms}`\nUptime: `{uptime()}`")
 
 
 
