d117fd9f85705116cb93a4ea58c2075f9b7f3b47
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2021-11-21 00:15:43 +0530

    Create autosleep.py

---
+++ tronx/modules/autosleep.py
@@ -0,0 +1,16 @@
+from pyrogram import filters
+
+from tronx import (
+	app,
+)
+
+from tronx.helpers import (
+	gen
+)
+
+
+
+@app.on_user_status(filters.me)
+async def sleep_when_offline(_, m):
+	await asyncio.sleep(10)
+	print("Client is sleeping for 10 seconds, because owner is offline !")
