9e2e137045e7ebe663702e69f98b9f30c641fce2
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2021-11-21 12:20:33 +0530

    Update utils.py

--- tronx/plugins/utils.py
+++ tronx/plugins/utils.py
@@ -13,7 +13,14 @@ from tronx.helpers import (
 
 @bot.on_message(filters.command("id"))
 async def id_of_user(_, m):
-	await bot.send_message(
-		m.chat.id,
-		f"**YOUR ID:** `{m.from_user.id}`\n**CHAT ID:** `{m.chat.id}`"
-	)
+	reply = m.reply_to_message
+	if not reply:
+		await bot.send_message(
+			m.chat.id,
+			"**YOUR ID:** `{m.from_user.id}`\n**CHAT ID:** `{m.chat.id}`"
+		)
+	elif reply:
+		await bot.send_message(
+			m.chat.id,
+			"**YOUR ID:** `{m.from_user.id}`\n**CHAT ID:** `{m.chat.id}`\n**REPLIED ID:** `{reply.from_user.id}`"
+		)
