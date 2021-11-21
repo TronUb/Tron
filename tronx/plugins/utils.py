7ae24f03a689b5c187a2ed76da36cf3ddc4cdd22
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2021-11-21 12:16:04 +0530

    Update utils.py

--- tronx/plugins/utils.py
+++ tronx/plugins/utils.py
@@ -15,5 +15,5 @@ from tronx.helpers import (
 async def id_of_user(_, m):
 	await bot.send_message(
 		m.chat.id,
-		f"**YOUR ID:** {m.from_user.id}\n**CHAT ID:** {m.chat.id}"
+		f"**YOUR ID:** `{m.from_user.id}`\n**CHAT ID:** `{m.chat.id}`"
 	)
