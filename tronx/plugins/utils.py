1535645d55c23bea4889555bfe15665e23f25299
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2021-11-21 12:10:38 +0530

    Create utils.py

---
+++ tronx/plugins/utils.py
@@ -0,0 +1,19 @@
+from pyrogram import filters
+
+from tronx import (
+	bot
+)
+
+from tronx.helpers import (
+	error,
+)
+
+
+
+
+@bot.on_message(filters.command("id"))
+async def id_of_user(_, m):
+	await app.send_message(
+		m.chat.id,
+		f"**YOUR ID:** {m.from_user.id}\n**CHAT ID:** {m.chat.id}"
+	)
