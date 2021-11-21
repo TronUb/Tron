0e664669572be59eb6d63ad1b26e26a82d9dba91
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2021-11-21 23:25:35 +0530

    Update botfun.py

--- tronx/plugins/botfun.py
+++ tronx/plugins/botfun.py
@@ -31,3 +31,25 @@ async def increment(_, m):
 				m.chat.id,
 				f"{reply.from_user.first_name}: 1 increment"
 			) 
+
+
+
+
+@bot.on_message(filters.command("1", "-"))
+async def increment(_, m):
+	reply = m.reply_to_message
+	if reply:
+		if str(reply.from_user.id) in collect:
+			data = collect.get(str(reply.from_user.id)) 
+			collect.update({str(reply.from_user.id) : str(int(data) - 1)})
+			await bot.send_message(
+				m.chat.id,
+				f"{reply.from_user.first_name}: " + str(int(data) - 1) + " decrement"
+			)
+		elif str(reply.from_user.id) not in collect:
+			data = {str(reply.from_user.id) : str(1)}
+			collect.update(data)
+			await bot.send_message(
+				m.chat.id,
+				f"{reply.from_user.first_name}: 1 decrement"
+			) 
