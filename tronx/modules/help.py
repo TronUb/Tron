366ad27f6a8a0b3252d0ce3636eaf627b89fa8e0
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2021-11-23 21:45:12 +0530

    Update help.py

--- tronx/modules/help.py
+++ tronx/modules/help.py
@@ -81,6 +81,7 @@ async def help_menu(app, m):
 					disable_notification=True, 
 					hide_via=True
 				)
+				print(data)
 			else:
 				await send_edit(m, "Please check your bots inline mode is on or not . . .", delme=3, mono=True)
 		elif args:
