2c34b98638e3ec19d654c1a32e8acd30280bc1c5
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2021-11-24 13:16:07 +0530

    Create on_message.py

---
+++ tronx/methods/decorators/on_message.py
@@ -0,0 +1,50 @@
+from typing import Callable
+
+import pyrogram
+from pyrogram.filters import Filter
+from pyrogram.scaffold import Scaffold
+
+
+class OnMessage(Scaffold):
+    def on_msg(
+        self=None,
+        info : dict = None,
+        filters=None,
+        group: int = 0
+    ) -> callable:
+        """Decorator for handling messages.
+
+        This does the same thing as :meth:`~pyrogram.Client.add_handler` using the
+        :obj:`~pyrogram.handlers.MessageHandler`.
+
+        Parameters:
+            filters (:obj:`~pyrogram.filters`, *optional*):
+                Pass one or more filters to allow only a subset of messages to be passed
+                in your function.
+
+            info (dict, *optional)
+                command name and information about this command.
+
+            group (``int``, *optional*):
+                The group identifier, defaults to 0.
+        """
+
+        CMD_HELP.update(info)
+
+        def decorator(func: Callable) -> Callable:
+            if isinstance(self, pyrogram.Client):
+                self.add_handler(pyrogram.handlers.MessageHandler(func, filters), group)
+            elif isinstance(self, Filter) or self is None:
+                if not hasattr(func, "handlers"):
+                    func.handlers = []
+
+                func.handlers.append(
+                    (
+                        pyrogram.handlers.MessageHandler(func, self),
+                        group if filters is None else filters
+                    )
+                )
+
+            return func
+
+        return decorator
