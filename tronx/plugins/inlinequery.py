da3af41b54229d29b79db1c1eb1d20b2645484ec
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2021-11-28 14:23:07 +0530

    Update inlinequery.py

--- tronx/plugins/inlinequery.py
+++ tronx/plugins/inlinequery.py
@@ -1,4 +1,76 @@
+import time
+import heroku3
+import requests
 
+from sys import (
+	version_info, 
+	platform,
+)
+
+from pyrogram import (
+	Client, 
+	filters, 
+	__version__ as __pyro_version__,
+)
+
+from pyrogram.types import (
+	InlineKeyboardButton, 
+	InlineKeyboardMarkup, 
+	InlineQueryResultArticle, 
+	InlineQueryResultPhoto, 
+	InputTextMessageContent, 
+	CallbackQuery, 
+	Message,
+)
+
+try:
+	from tronx import bot
+except ImportError:
+	bot = None
+
+from tronx import (
+	app, 
+	CMD_HELP, 
+	version, 
+	USER_ID, 
+	USER_NAME, 
+	USER_USERNAME, 
+	Config,
+	uptime,
+	PREFIX,
+	__python_version__,
+	db_status,
+	lara_version,
+)
+
+from tronx.helpers import (
+	helpdex,
+	build_keyboard,
+	quote,
+	data,
+	ialive_pic,
+	bot_bio,
+	bot_pic,
+)
+
+from tronx.database.postgres import pmpermit_sql as db
+from tronx.database.postgres import dv_sql as dv
+from tronx.variable import message_ids
+
+
+
+# variables
+USER_ID = [USER_ID]
+
+PIC = "https://telegra.ph/file/38eec8a079706b8c19eae.mp4"
+
+settings = build_keyboard((["• Settings •", "open-settings-dex"], ["• Modules •", "tron-dex-2"]))
+extra = build_keyboard((["• Extra •", "open-extra-dex"], ["• Stats •", "open-stats-dex"]))
+about = build_keyboard(([["About", "open-about-dex"]]))
+close = build_keyboard(([["Close", "close-dex"]]))
+approve = build_keyboard(([["Approve", "approve-user"]]))
+global_command = build_keyboard(([["• Global commands •", "global-commands"]]))
+home_back = build_keyboard((["Home", "close-dex"], ["Back", "open-start-dex"]))
 
 
 
