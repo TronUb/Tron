96b2ef193dd4ddf5d37802013b3ed4002e0aecb3
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2021-11-28 14:50:12 +0530

    Update start.py

--- tronx/plugins/start.py
+++ tronx/plugins/start.py
@@ -1,25 +1,9 @@
-import time
-import heroku3
-import requests
-
-from sys import (
-	version_info, 
-	platform,
-)
-
 from pyrogram import (
-	Client, 
 	filters, 
-	__version__ as __pyro_version__,
 )
 
 from pyrogram.types import (
-	InlineKeyboardButton, 
 	InlineKeyboardMarkup, 
-	InlineQueryResultArticle, 
-	InlineQueryResultPhoto, 
-	InputTextMessageContent, 
-	CallbackQuery, 
 	Message,
 )
 
@@ -29,32 +13,15 @@ except ImportError:
 	bot = None
 
 from tronx import (
-	app, 
-	CMD_HELP, 
-	version, 
 	USER_ID, 
-	USER_NAME, 
-	USER_USERNAME, 
-	Config,
-	uptime,
-	PREFIX,
-	__python_version__,
-	db_status,
-	lara_version,
 )
 
 from tronx.helpers import (
-	helpdex,
 	build_keyboard,
-	quote,
-	data,
-	ialive_pic,
 	bot_bio,
 	bot_pic,
 )
 
-from tronx.database.postgres import pmpermit_sql as db
-from tronx.database.postgres import dv_sql as dv
 from tronx.variable import message_ids
 
 
