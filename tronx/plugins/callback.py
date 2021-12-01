e2d397c812f901aff8697ae118c891e5f9493abc
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2021-12-01 18:48:26 +0530

    Update callback.py

--- tronx/plugins/callback.py
+++ tronx/plugins/callback.py
@@ -152,7 +152,7 @@ async def give_plugin_cmds(_, cb):
 
 # list of helpdex
 @alert_user
-@bot.on_callback_query(filters.regex("open-stats-dex") & filters.user(USER_ID))
+@bot.on_callback_query(filters.regex("open-stats-dex"))
 async def _stats(_, cb):
 	await cb.edit_message_text(
 		text=f"**Dex:** Stats\n\n**Location:** /home/stats\n\nName: {USER_NAME}\nLara version: {lara_version}\nPython version: {__python_version__}\nPyrogram: {__pyro_version__}\nDB_URI: {db_status}\nUptime: {uptime()}\n\nUser Bio: {Config.USER_BIO}",
