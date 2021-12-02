df9aa0b70b3f72a7d71bd1699c47f12da429ffc5
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2021-12-02 19:56:40 +0530

    Update utube.py

--- tronx/modules/utube.py
+++ tronx/modules/utube.py
@@ -23,12 +23,13 @@ async def utube_info(_, m: Message):
 	elif not reply and long(m) == 1:
 		return await send_edit(m, "Reply to youtube link or give link as a suffix . . .", mono=True, delme=5)
 
+	await send_edit(m, "Getting information . . .", mono=True)
 	yt = YouTube(link)
 	thumb_link = yt.thumbnail_url
-	data = f"**Title:** {yt.title}\n"
-	data += f"**Duration:** {get_readable_time(yt.length)}\n"
-	data += f"**Description:** {yt.description}\n"
-	data += f"**Views:** {yt.views}\n"
+	data = f"**Title:** {yt.title}\n\n"
+	data += f"**Duration:** {get_readable_time(yt.length)}\n\n"
+	data += f"**Description:** {yt.description}\n\n"
+	data += f"**Views:** {yt.views}\n\n"
 	data += f"**Age Restricted:** {'Yes' if yt.age_restricted else 'No'}"
 
 	await app.send_photo(m.chat.id, thumb_link, caption=data)
