352e17cbfc97916821402b98f986850292cce288
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2021-12-05 21:49:25 +0530

    Update dv.py

--- tronx/modules/dv.py
+++ tronx/modules/dv.py
@@ -118,3 +118,22 @@ async def get_database_var(_, m: Message):
 	else:
 		await send_edit(m, "Something went wrong, please try again later !", mono=True)
 
+
+
+
+@app.on_message(gen("alldv"))
+async def get_all_dv(_, m: Message):
+	if bool(dv.get_alldv()) is True:
+		my_dict = dv.get_alldv()
+		dict_data = []
+		dict_data.clear()
+
+		for key, value in my_dict:
+			dict_data.append(f"{key} = {value}\n")
+
+		await send_edit(m, "All Database vars:\n\n" + "".join(dict_data))
+	else:
+		await send_edit(m, "There are no database vars (empty) . . .", mono=True, delme=5)
+
+
+
