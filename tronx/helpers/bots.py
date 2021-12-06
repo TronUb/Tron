971f35f4c2e0a0690aac7a29a888657bd839e20b
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2021-12-06 22:57:40 +0530

    Update bots.py

--- tronx/helpers/bots.py
+++ tronx/helpers/bots.py
@@ -1,5 +1,3 @@
-from tronx.helpers.utils import mention_markdown 
-
 from tronx import (
 	BOT_ID,
 	BOT_NAME,
@@ -13,38 +11,37 @@ from pyrogram.types import Message
 
 
 
-# name of bot (1st priority to database variable)
 def botname():
+	"""Get your bot name"""
 	var = dv.getdv("BOT_NAME")
 	var_data = var if bool(var) is True else Config.BOT_NAME
 	data = var_data if var_data else BOT_NAME
 	return data if data else None
 
 
-# username of bot
 def botusername():
+	"""Get your bot username"""
 	var = dv.getdv("BOT_USERNAME")
 	var_data = var if bool(var) is True else Config.BOT_USERNAME
 	data = var_data if var_data else BOT_USERNAME
 	return data if data else None
 
 
-# mention of bot
 def botmention():
-	return mention_markdown(botid(), botname()) if botid() and botname() else None  
+	"""Get bot mention"""
+	return f"[{botname()}](tg://user?id={botid()}) if botid() and botname() else None  
 
 
-# id of bot 
 def botid():
+	"""Get your bots telegram id"""
 	var = dv.getdv("BOT_ID")
 	var_data = var if bool(var) is True else Config.BOT_ID
 	data = var_data if var_data else BOT_ID
 	return data if data else None
 
 
-
-# bio of bot
 def bot_bio(m: Message):
+	"""Get your bots bio"""
 	msg = f"Hey {m.from_user.mention} my name is LARA and I am your assistant bot. I can help you in many ways . Just use the buttons below to get list of possible commands.\n\nCatagory: "  
 	var = dv.getdv("BOT_BIO")
 	var_data = var + "\n\nCatagory: " if bool(var) else Config.BOT_BIO + "\n\nCatagory: "
@@ -52,10 +49,8 @@ def bot_bio(m: Message):
 	return data if data else None
 
 
-
-
-# pic of bot
 def bot_pic():
+	"""Get your bot pic url"""
 	var = dv.getdv("BOT_PIC")
 	var_data = var if bool(var) else Config.BOT_PIC
 	data = var_data if bool(var_data) else False
