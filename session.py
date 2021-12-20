c338c770e6fb237c216f386ccc1bad7fbad5fd9a
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2021-12-20 11:06:44 +0530

    Update session.py

--- session.py
+++ session.py
@@ -1,20 +1,43 @@
-import tgcrypto
 import os
-import asyncio
+os.system("pip3 install tgcrypto pyrogram")
 
 from pyrogram import Client
+import asyncio
 
 # guide them 
-print("@Tronuserbot Corporation\n\nGet the following values by logging to\nhttps://my.telegram.org\n\nRequired:\n\n1.APP_ID\n2.API_HASH\n3.PHONE NUMBER\n(WITH COUNTRY CODE)\n\n")
 
-# integer only
-API_ID = int(input("Enter APP_ID: "))
+intro = """
+@Tronuserbot Corporation
+Get the following values by logging to,
 
-# string only
-API_HASH = input("Enter API HASH: ")
+https://my.telegram.org
+
+Required:
+  
+  1. APP_ID
+  2. API_HASH
+  3. PHONE NUMBER (WITH COUNTRY CODE)
+  
+"""
+
+
+print(intro)
+
+
+# decimal only
+while not isinstance(input("\nEnter your API_ID: "), int):
+  print("\n\nPlease enter a digit.\n\n")
+  API_ID = input("Enter your API_ID (1234567): ")
+
+
+
+# hexadecimal number
+API_HASH = input("\nEnter API HASH: ")
+
+
 
 # create a new pyrogram session
-with Client(":memory:", api_id=API_ID, api_hash=API_HASH, hide_password=True) as app:
-	app.send_message("me", f"This Is Your Tron Userbot • [ `SESSION` ]\n\n```{app.export_session_string()}```\n\n⚠️• Don't share this with anyone !!\n\nCreate Again • [ Press Here](https://replit.com/@beastzx18/Tron-Userbot-Session)", disable_web_page_preview=True,) 
-	print("Your String Session Is Successfully Saved In Telegram Saved Messages !! Don't Share It With Anyone!!", Anyone having your session can use your telegram account !)
-	
+with Client(":memory:", api_id=int(API_ID), api_hash=API_HASH, hide_password=True) as app:
+  app.send_message("me", f"This Is Your Tron Userbot • [ `SESSION` ]\n\n```{app.export_session_string()}```\n\n⚠️• Don't share this with anyone !!\n\nCreate Again • [ Press Here](https://replit.com/@beastzx18/Tron-Userbot-Session)", disable_web_page_preview=True,) 
+  print("Your String Session Is Successfully Saved In Telegram Saved Messages !! Don't Share It With Anyone!! Anyone having your session can use your telegram account !")
+  
