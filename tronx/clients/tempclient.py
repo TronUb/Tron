d1f7289338bae26730bc15a3fd653cfe1f2901dc
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2022-01-20 22:37:58 +0530

    Update tempclient.py

--- tronx/clients/tempclient.py
+++ tronx/clients/tempclient.py
@@ -1,8 +1,15 @@
 import os
+import sys
 import platform
 import subprocess
 
 
+
+
+if sys.version_info[0] < 3 or sys.version_info[1] < 9:
+	""" lower version will produce errors in userbot """
+	log.error("python version 3.9.0 or greater is required, bot is quitting !")
+	quit(1)
 
 
 if list(platform.uname())[1] == "localhost":
@@ -40,6 +47,16 @@ from telegraph import Telegraph
 
 
 
+if not Config.LOG_CHAT:
+	""" log chat is required """
+	log.warning("LOG_CHAT is important for bots normal working, please fill it, quitting.")
+	quit(1)
+
+
+if not Config.DB_URI:
+	""" database is required """
+	log.warning("DB_URI is important please fill this requirement, quitting.")
+	quit(1)
 
 
 
