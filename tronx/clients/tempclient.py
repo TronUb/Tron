47050915987c1508d7837b347865eed70cc5326a
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2022-01-20 22:57:16 +0530

    Update tempclient.py

--- tronx/clients/tempclient.py
+++ tronx/clients/tempclient.py
@@ -61,6 +61,17 @@ if not Config.DB_URI:
 
 
 
+class Collector(Methods, Config):
+	# versions /
+
+	userbot_version = "v.0.0.5"
+	assistant_version = "v.0.0.1"
+	python_version = str(platform.python_version())
+	pyrogram_version = str(pyrogram.__version__)
+
+
+
+
 
 class Initialisation(Collector):
 	"""tempclients initialisation"""
@@ -85,3 +96,122 @@ class Initialisation(Collector):
 	# stop
 	tempapp.stop()
 	tempbot.stop()
+
+
+
+
+
+class utils(Initialisation):
+	def clear():
+		""" clear terminal logs """
+		subprocess.call("clear" if os.name == "posix" else "cls") 
+
+
+	def add_user(self, user_id: Union[int, List[int]], chat_id: str):
+		""" Add users in groups / channels """
+		try:
+			done = app.add_chat_members(
+				chat_id, 
+				user_id
+			)
+			return True if done else False
+		except Exception as e:
+			print(e)
+
+
+	def exists(self, user_id: int, chat_id: str):
+		_data = []
+		_data.clear()
+		lime = app.get_chat_members(chat_id)
+
+		for x in lime:
+			_data.append(x.user.id)
+		return True if user_id in _data else False
+
+
+	def check_bot_in_log_chat(self):
+		try:
+			if bot:
+				print("Checking presence of bot in log chat . . .\n")
+				try:
+					if self.exists(BOT_ID, LOG_CHAT) is False:
+						self.add_user(
+							LOG_CHAT,
+							BOT_ID
+						)
+						print(f"Added bot in log chat . . .\n")
+					else:
+						print(f"Bot is present in log chat . . .\n")
+				except PeerIdInvalid:
+					print("Peer id is invalid, Manually send a message in log chat . . .\n")
+					pass
+			else:
+				log.warning("Bot is not available, please check (TOKEN, API_ID, API_HASH)")
+		except Exception as e:
+			print(e)
+
+
+	def get_readable_time(seconds: int) -> str:
+		count = 0
+		ping_time = ""
+		time_list = []
+		time_suffix_list = ["s", "m", "h", "days"]
+
+		while count < 4:
+			count += 1
+			remainder, result = divmod(seconds, 60) if count < 3 else divmod(seconds, 24)
+			if seconds == 0 and remainder == 0:
+				break
+			time_list.append(int(result))
+		seconds = int(remainder)
+
+		for x in range(len(time_list)):
+			time_list[x] = str(time_list[x]) + time_suffix_list[x]
+		if len(time_list) == 4:
+			ping_time += time_list.pop() + ", "
+
+		time_list.reverse()
+		ping_time += ":".join(time_list)
+
+		return ping_time
+
+
+	def uptime(self):
+		""" Bot active time """
+		return self.get_readable_time(time.time() - self.StartTime)
+
+
+
+
+
+
+
+	# database /
+
+	db_status = "Available" if Config.DB_URI else "Not Available"
+
+	# containers /
+
+	CMD_HELP = {}
+	HELP = {}
+
+	# owner details /
+
+	OWNER_NAME = "࿇•ẞᗴᗩSԵ•࿇"
+	OWNER_ID = 1790546938
+	OWNER_USERNAME = "@BEASTZX"
+
+	# other /
+
+	Repo = "https://github.com/beastzx18/Tron"
+	StartTime = time.time()
+
+	# debugging /
+
+	logging.basicConfig(level=logging.WARNING)
+	log = logging.getLogger(__name__)
+
+	# telegraph /
+
+	telegraph = Telegraph()
+	telegraph.create_account(short_name=USER_NAME if USER_NAME else "Tron userbot")
