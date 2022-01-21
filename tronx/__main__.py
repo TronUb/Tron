40402a9b4446768fd99fef22d51ce1002d34bd10
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2022-01-21 08:58:58 +0530

    Update __main__.py

--- tronx/__main__.py
+++ tronx/__main__.py
@@ -1,13 +1,6 @@
-import os
-import importlib
 import asyncio
-
 from pyrogram import idle
-from tronx.client import (
-	app, 
-	bot,
-	log,
-)
+from tronx.client import app
 
 
 
@@ -15,47 +8,15 @@ from tronx.client import (
 loop = asyncio.get_event_loop()
 
 
-def import_module(path, exclude=[], display_module=True):
-	"""Modified version of pyrogram smart plugins"""
-	bin = []
-	bin.clear()
-
-	if not os.path.exists(path):
-		return print(f"No path found: {path}")
-
-	plugins = []
-	for x in os.listdir(path):
-		if x.endswith(".py"):
-			if not x in ["__pycache__",  "__init__.py"]:
-				plugins.append(x.replace(".py", ""))
-
-	py_path_raw = ".".join(path.split("/"))
-	py_path = py_path_raw[0:len(py_path_raw)-1]
-
-	count = 0
-	for x in plugins:
-		if not x in exclude:
-			importlib.import_module(py_path + "." + x)
-			count += 1
-			bin.append(x)
-
-	if display_module:
-		data = sorted(bin)
-		for x in data:
-			print(x + " Loaded !")
-	return count
-
-
-
 
 async def start_assistant():
 	""" Start assistant """
-	if bot:
-		await bot.start()
-		log.info("Assistant activated, startup in progress . . .\n")
+	if app.bot:
+		await app.bot.start()
+		app.log.info("Assistant activated, startup in progress . . .\n")
 	else:
-		log.info("Assistant start unsuccessful, please check that you have given the bot token.\n")
-		log.info("skipping assistant start !")
+		app.log.info("Assistant start unsuccessful, please check that you have given the bot token.\n")
+		app.log.info("skipping assistant start !")
 
 
 
@@ -64,10 +25,10 @@ async def start_userbot():
 	""" Start userbot """
 	if app:
 		await app.start()
-		log.info("Userbot activated, startup in progress . . .\n")
+		app.log.info("Userbot activated, startup in progress . . .\n")
 	else:
-		log.info("Userbot startup unsuccessful, please check everything again ...")
-		log.info("Couldn't load modules of userbot")
+		app.log.info("Userbot startup unsuccessful, please check everything again ...")
+		app.log.info("Couldn't load modules of userbot")
 
 
 
@@ -77,10 +38,10 @@ async def start_bot():
 	print("___________________________________. Welcome to Tron corporation .___________________________________\n\n\n")
 	print("initialising . . .\n\n")
 	print("Loading plugins:\n\n")
-	_plugs = import_module("tronx/plugins/")
+	_plugs = app.import_module("tronx/plugins/")
 	print(f"\n\n{_plugs} plugins Loaded\n\n")
 	print("Loading modules:\n\n")
-	_mods = import_module("tronx/modules/")
+	_mods = app.import_module("tronx/modules/")
 	print(f"\n\n{_mods} modules Loaded")
 	await start_assistant()
 	await start_userbot()
