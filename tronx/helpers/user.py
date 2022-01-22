e74f92005e8e91917a8a5e6f9eeb959fdd1bd08e
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2022-01-22 10:55:37 +0530

    Update user.py

--- tronx/helpers/user.py
+++ tronx/helpers/user.py
@@ -1,7 +1,3 @@
-from tronx import app
-
-from tronx.helpers.utils import mention_markdown
-
 from tronx.database.postgres import dv_sql as dv
 
 
@@ -9,35 +5,35 @@ from tronx.database.postgres import dv_sql as dv
 
 class User(object):
 	# instant change of names through database vars
-	def UserName():
+	def UserName(self):
 		var = dv.getdv("USER_NAME")
-		one = var if bool(var) is True else app.USER_NAME
-		two = one if one else app.REAL_USER_NAME
+		one = var if bool(var) is True else self.USER_NAME
+		two = one if one else self.REAL_USER_NAME
 		return two if two else None
 
 
 	# username of bot owner
-	def UserUsername():
+	def UserUsername(self):
 		var = dv.getdv("USER_USERNAME")
-		one = var if bool(var) is True else app.USER_USERNAME
-		two = one if one else app.REAL_USER_USERNAME
+		one = var if bool(var) is True else self.USER_USERNAME
+		two = one if one else self.REAL_USER_USERNAME
 		return two if two else None
 
 
 	# mention of bot owner
-	def UserMention():
-		return mention_markdown(UserId(), UserName()) if UserName() and UserId() else None
+	def UserMention(self):
+		return self.mention_markdown(self.UserId(), self.UserName()) if self.UserName() and self.UserId() else None
 
 
 	# telegram id of bot owner
-	def UserId():
+	def UserId(self):
 		var = dv.getdv("USER_ID")
-		one = var if bool(var) is True else app.USER_ID
-		two = one if one else app.REAL_USER_ID
+		one = var if bool(var) is True else self.USER_ID
+		two = one if one else self.REAL_USER_ID
 		return two if two else None
 
 
 	# dc id of bot owner
-	def UserDc():
-		data = app.USER_DC if app.USER_DC else app.REAL_USER_DC
+	def UserDc(self):
+		data = self.USER_DC if self.USER_DC else self.REAL_USER_DC
 		return data if data else None
