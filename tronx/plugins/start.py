4fde82d5441c282da1fcc35b990200237dda4519
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2022-01-22 17:57:59 +0530

    Update start.py

--- tronx/plugins/start.py
+++ tronx/plugins/start.py
@@ -1,33 +1,28 @@
 import time
 
-from tronx import (
-	bot,
-	BOT_ID,
-)
+from tronx import app
 
 from pyrogram import filters
 from pyrogram.types import Message
 
-from tronx.helpers import error
 
 
 
-
-@bot.on_message(filters.command("start"))
+@app.bot.on_message(filters.command("start"))
 async def send_help(_, m: Message):
 	await m.reply("How can i help you ?")
 
 
 
-@bot.on_message(filters.new_chat_members & filters.group)
-async def added_to_group_msg(_, m):
-	if m.new_chat_members[0].id == BOT_ID:
+@app.bot.on_message(filters.new_chat_members & filters.group)
+async def added_to_group_msg(_, m: Message):
+	if m.new_chat_members[0].id == app.REAL_BOT_ID:
 		try:
-			await bot.send_message(
+			await app.bot.send_message(
 				m.chat.id,
 				"Thank You for adding me in this group !\nUse /help to know my features."
 			)
 		except Exception as e:
-			await error(m, e)
+			await app.error(m, e)
 	else:
 		return
