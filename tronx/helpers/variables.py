81a107954a85629389d1ea1c3b38e24aefdc195f
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2022-01-22 10:29:03 +0530

    Update variables.py

--- tronx/helpers/variables.py
+++ tronx/helpers/variables.py
@@ -1,24 +1,14 @@
-from tronx import (
-	USER_ID,
-	PREFIX,
-)
+class Variables(object):
+	# lower
+	message_ids = {}
 
+	# assistant
+	assistant_name = "Nora"
+	assistant_age = "19"
+	assistant_gender = "Female"
 
+	# userbot
+	userbot_name = "Tron"
 
-# upper
-USER_ID = [USER_ID]
-CMD_HELP = {}
-
-# lower
-message_ids = {}
-
-# assistant
-assistant_name = "Nora"
-assistant_age = "19"
-assistant_gender = "Female"
-
-# userbot
-userbot_name = "Tron"
-
-# photo
-PIC = "https://telegra.ph/file/38eec8a079706b8c19eae.mp4"
+	# photo
+	PIC = "https://telegra.ph/file/38eec8a079706b8c19eae.mp4"
