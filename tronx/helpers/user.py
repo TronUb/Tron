80f6e1fa863879f0f3c6a8d6cb27686b9a382258
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2022-01-22 10:41:06 +0530

    Update user.py

--- tronx/helpers/user.py
+++ tronx/helpers/user.py
@@ -1,10 +1,4 @@
-from tronx import (
-	USER_NAME,
-	USER_USERNAME,
-	USER_ID,
-	USER_DC,
-	Config,
-	)
+from tronx import app
 
 from tronx.helpers.utils import mention_markdown
 
@@ -13,36 +7,37 @@ from tronx.database.postgres import dv_sql as dv
 
 
 
-# instant change of names through database vars
-def myname():
-	var = dv.getdv("USER_NAME")
-	one = var if bool(var) is True else Config.USER_NAME
-	two = one if one else USER_NAME
-	return two if two else None
+class User(object):
+	# instant change of names through database vars
+	def UserName():
+		var = dv.getdv("USER_NAME")
+		one = var if bool(var) is True else app.USER_NAME
+		two = one if one else app.REAL_USER_NAME
+		return two if two else None
 
 
-# username of bot owner
-def myusername():
-	var = dv.getdv("USER_USERNAME")
-	one = var if bool(var) is True else Config.USER_USERNAME
-	two = one if one else USER_USERNAME
-	return two if two else None
+	# username of bot owner
+	def UserUsername():
+		var = dv.getdv("USER_USERNAME")
+		one = var if bool(var) is True else app.USER_USERNAME
+		two = one if one else app.REAL_USER_USERNAME
+		return two if two else None
 
 
-# mention of bot owner
-def mymention():
-	return mention_markdown(myid(), myname()) if myname() and myid() else None
+	# mention of bot owner
+	def UserMention():
+		return mention_markdown(UserId(), UserName()) if UserName() and UserId() else None
 
 
-# telegram id of bot owner
-def myid():
-	var = dv.getdv("USER_ID")
-	one = var if bool(var) is True else Config.USER_ID
-	two = one if one else USER_ID
-	return two if two else None
+	# telegram id of bot owner
+	def UserId():
+		var = dv.getdv("USER_ID")
+		one = var if bool(var) is True else app.USER_ID
+		two = one if one else app.REAL_USER_ID
+		return two if two else None
 
 
-# dc id of bot owner
-def mydc():
-	data = Config.USER_DC if Config.USER_DC else USER_DC
-	return data if data else None
+	# dc id of bot owner
+	def UserDc():
+		data = app.USER_DC if app.USER_DC else app.REAL_USER_DC
+		return data if data else None
