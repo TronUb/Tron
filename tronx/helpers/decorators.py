8927bf37ad67c476564d3d44ed80830b014fc86f
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2022-01-22 10:26:26 +0530

    Update decorators.py

--- tronx/helpers/decorators.py
+++ tronx/helpers/decorators.py
@@ -1,22 +1,21 @@
 from pyrogram.types import CallbackQuery
-from .variables import USER_ID
 from pyrogram.errors import MessageNotModified
 
 
 
 
 
-
-def alert_user(func):
-	async def wrapper(_, cb: CallbackQuery):
-		if cb.from_user and not cb.from_user.id in USER_ID:
-			await cb.answer(
-				f"Sorry, but you can't use this userbot ! make your own userbot at @tronuserbot", 
-				show_alert=True
-			)
-		else:
-			try:
-				await func(_, cb)
-			except MessageNotModified:
-				pass
-	return wrapper
+class Decorators(object):
+	def alert_user(self, func):
+		async def wrapper(_, cb: CallbackQuery):
+			if cb.from_user and not cb.from_user.id in self.USER_ID:
+				await cb.answer(
+					f"Sorry, but you can't use this userbot ! make your own userbot at @tronuserbot", 
+					show_alert=True
+				)
+			else:
+				try:
+					await func(_, cb)
+				except MessageNotModified:
+					pass
+		return wrapper
