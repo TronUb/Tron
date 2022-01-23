1f9d3f2013462f003c2a0cd40469bc594d31911f
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2022-01-23 20:58:39 +0530

    Update __init__.py

--- tronx/database/postgres/__init__.py
+++ tronx/database/postgres/__init__.py
@@ -2,13 +2,14 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
-from tronx import Config
 
 
 
 
 def start() -> scoped_session:
-	engine = create_engine(Config.DB_URI)
+	from tronx.clients import app
+
+	engine = create_engine(app.DB_URI)
 	BASE.metadata.bind = engine
 	BASE.metadata.create_all(engine)
 	return scoped_session(sessionmaker(bind=engine, autoflush=False))
