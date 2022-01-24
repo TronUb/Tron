81b07091778b325de077f1f9b7adb3d318c32015
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2022-01-24 19:01:46 +0530

    Update filetools.py

--- tronx/modules/filetools.py
+++ tronx/modules/filetools.py
@@ -1,39 +1,24 @@
 import os
-import shutil
+import sys
 import time
+import shutil
 import asyncio
 import zipfile
-import sys
 from pathlib import Path
 
 from pyrogram.types import Message
 from pyrogram import filters, Client
 
-from tronx import (
-	app, 
-	CMD_HELP,
-	Config,
-	PREFIX,
-	USER_NAME, 
-	USER_ID,
-	)
+from tronx import app
 
 from tronx.helpers import (
 	gen,
-	error,
-	send_edit,
-	delete,
-	long,
-	types,
-	mymention,
-	create_file,
 )
 
 
 
 
-
-CMD_HELP.update(
+app.CMD_HELP.update(
 	{"zip" : (
 		"zip",
 		{
@@ -47,8 +32,6 @@ CMD_HELP.update(
 
 
 
-
-#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 def zipdir(dirName):
 	dice = []
@@ -67,22 +50,20 @@ async def unzipfiles(zippath):
 
 
 
-#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
 
 @app.on_message(gen("zip"))
 async def zipit(_, m: Message):
 	reply = m.reply_to_message
 	if not reply:
-		return await send_edit(m, f"Reply to some media file . . .", mono=True, delme=2)
+		return await app.send_edit(m, f"Reply to some media file . . .", mono=True, delme=2)
 
 	elif reply:
-		if reply.text:
-			return await send_edit(m, "Reply to some media not text . . .", mono=True)
-		await send_edit(m, "Zipping . . .", mono=True)
+		if not reply.media:
+			return await app.send_edit(m, "Reply to some media not text . . .", mono=True)
+		await app.send_edit(m, "Zipping . . .", mono=True)
 
-		if Config.TEMP_DICT:
-			loc = Config.TEMP_DICT
+		if app.TEMP_DICT:
+			loc = app.TEMP_DICT
 		else:
 			loc = "tronx/downloads"
 		dl = await app.download_media(
@@ -91,28 +72,25 @@ async def zipit(_, m: Message):
 		)
 		zipfile.ZipFile(dl.replace("/app/downloads/", "") + ".zip", "w").write(dl)
 		place = dl.replace("/app/downloads/", "") + ".zip"
-		await send_edit(m, f"**Your file is compressed and saved here:** \n`{place}`")
+		await app.send_edit(m, f"**Your file is compressed and saved here:** \n`{place}`")
 	else:
-		await send_edit(m, "Something went wrong . . .", delme=2, mono=True)
+		await app.send_edit(m, "Something went wrong . . .", delme=2, mono=True)
 
 
-#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 @app.on_message(gen("unzip"))
 async def unzipit(_, m: Message):
-	if long(m) == 2:
-		if long(m) <= 4096:
+	if app.long(m) == 2:
+		if app.long(m) <= 4096:
 			loc = m.text.split(None, 1)[1]
-			await send_edit(m, "Unzipping file . . .")
+			await app.send_edit(m, "Unzipping file . . .", mono=True)
 			extract_path = await unzipfiles(loc)
-			await send_edit(m, f"File unzipped and saved here: `{extract_path}`")
-			return
+			await app.send_edit(m, f"File unzipped and saved here: `{extract_path}`")
 		else:
-			await send_edit(m, "The path length exceeds 4096 character, please check again ...", delme=2, mono=True)
+			await app.send_edit(m, "Text is too long !", delme=2, mono=True)
 	else:
-		await send_edit(m, "Give me the file path to unzip the file ...", delme=2, mono=True)
-
+		await app.send_edit(m, "Give me the file path to unzip the file . . .", delme=2, mono=True)
 
 
 
@@ -120,41 +98,39 @@ async def unzipit(_, m: Message):
 @app.on_message(gen("new"))
 async def create_anyfile(app, m:Message):
 	reply = m.reply_to_message
-	await send_edit(m, "making file ...")
+	await app.send_edit(m, "making file . . .", mono=True)
 	cmd = m.command
 	try:
-		if long(m) < 4096 and long(m) > 2:
+		if app.long(m) < 4096 and app.long(m) > 2:
 			data = m.text.split(None, 2)[2]
 			givename = cmd[1]
-			await create_file(
+			await app.create_file(
 				m, 
 				filename=givename, 
 				text=data
 			)
 		# if replied to text without file name
-		elif long(m) == 1 and reply:
+		elif app.long(m) == 1 and reply:
 			data = reply.text
-			await create_file(
+			await app.create_file(
 				m, 
 				filename="file.py", 
 				text=data
 			)
 		# if replied to text with file name
-		elif long(m) > 1 and reply:
+		elif app.long(m) > 1 and reply:
 			givename = cmd[1]
 			data = reply.text
-			await create_file(
+			await app.create_file(
 				m, 
 				filename=givename, 
 				text=data
 			)
 		else:
-			await send_edit(
-				m, 
-				f"Use cmd correctly: `{PREFIX}new [ file name ] [content]`\n\nNote: use filename with extention, ex: file.py, file.txt, etc",
-				)
+			await app.send_edit(m, f"Use cmd correctly: `{app.PREFIX}new [ file name ] [content]`\n\nNote: use filename with extention, ex: file.py, file.txt, etc",)
 	except Exception as e:
-		await error(m, e)
+		await app.error(m, e)
+
 
 
 
