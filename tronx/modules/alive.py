fb40c6ff86604d197310b23f9cfd38aa63aa281a
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2022-01-24 17:48:36 +0530

    Update alive.py

--- tronx/modules/alive.py
+++ tronx/modules/alive.py
@@ -1,34 +1,16 @@
-import time
-
-from sys import version_info
-
 from pyrogram.errors import BotInvalid
 from pyrogram.types import Message
 
-from tronx import (
-	app,
-	CMD_HELP,
-	userbot_version, 
-	uptime,
-	Config,
-	python_version,
-	pyrogram_version,
-	)
+from tronx import app
 
 from tronx.helpers import (
 	gen,
-	error,
-	mymention,
-	send_edit,
-	BOT_USERNAME,
 )
 
-from tronx.database.postgres import dv_sql as db
 
 
 
-
-CMD_HELP.update(
+app.CMD_HELP.update(
 	{"alive" : (
 		"alive",
 		{
@@ -43,31 +25,31 @@ CMD_HELP.update(
 
 
 @app.on_message(gen("alive"))
-async def alive(app, m: Message):
+async def simple_alive(_, m: Message):
 	try:
-		await send_edit(m, ". . .")
+		await app.send_edit(m, ". . .", mono=True)
 
-		if db.getdv("USER_BIO"):
-			BIO = db.getdv("USER_BIO")
-		elif Config.USER_BIO:
-			BIO = Config.USER_BIO
+		if app.getdv("USER_BIO"):
+			BIO = app.getdv("USER_BIO")
+		elif app.USER_BIO:
+			BIO = app.USER_BIO
 		else:
 			BIO = False
 
 		alive_msg = f"\n"
 		if BIO:
 			alive_msg += f"⦿ {BIO}\n\n"
-		alive_msg += f"⟜ **Owner:** {mymention()}\n"
-		alive_msg += f"⟜ **Tron:** `{userbot_version}`\n"
-		alive_msg += f"⟜ **Python:** `{python_version}`\n"
-		alive_msg += f"⟜ **Pyrogram:** `{pyrogram_version}`\n"
-		alive_msg += f"⟜ **Uptime:** {uptime()}\n\n"
+		alive_msg += f"⟜ **Owner:** {app.mymention()}\n"
+		alive_msg += f"⟜ **Tron:** `{app.userbot_version}`\n"
+		alive_msg += f"⟜ **Python:** `{app.python_version}`\n"
+		alive_msg += f"⟜ **Pyrogram:** `{app.pyrogram_version}`\n"
+		alive_msg += f"⟜ **Uptime:** {app.uptime()}\n\n"
 
 		await m.delete()
-		if db.getdv("USER_PIC"):
-			pic = db.getdv("USER_PIC")
-		elif Config.USER_PIC:
-			pic = Config.USER_PIC
+		if app.getdv("USER_PIC"):
+			pic = app.getdv("USER_PIC")
+		elif app.USER_PIC:
+			pic = app.USER_PIC
 
 		if (pic) and (pic.endswith(".mp4" or ".mkv" or ".gif")):
 			await app.send_video(
@@ -91,25 +73,18 @@ async def alive(app, m: Message):
 				parse_mode="markdown",
 				)
 	except Exception as e:
-		await error(m, e)
+		await app.error(m, e)
 
 
 
 
 @app.on_message(gen("ialive"))
-async def inline_alive(app, m: Message):
-	await send_edit(m, "`...`")
+async def inline_alive(_, m: Message):
+	await app.send_edit(m, ". . .", mono=True)
 	try:
-		result = await app.get_inline_bot_results(
-			BOT_USERNAME, 
-			"#i2l8v3"
-		)
+		result = await app.get_inline_bot_results(app.bot.username, "#i2l8v3")
 	except BotInvalid:
-		return await send_edit(
-			m, 
-			"The bot can't be used in inline mode",
-			delme=2
-		)
+		return await app.send_edit(m, "The bot can't be used in inline mode", delme=2)
 
 	if result:
 		await app.send_inline_bot_result(
@@ -121,43 +96,31 @@ async def inline_alive(app, m: Message):
 		)
 		await m.delete()
 	else:
-		await send_edit(
-			m, 
-			"Something went wrong, please try again later . . .",
-			delme=2
-		)
+		await app.send_edit(m, "Something went wrong, please try again later . . .", delme=2)
 
 
 
 
 @app.on_message(gen(["qt"]))
-async def inline_alive(_, m: Message):
+async def inline_quote(_, m: Message):
 	try:
-		await send_edit(m,". . .")
+		await app.send_edit(m,". . .")
 		try:
 			result = await app.get_inline_bot_results(BOT_USERNAME, "#q7o5e")
 		except BotInvalid:
-			return await send_edit(m,"This bot can't be used in inline mode.", delme=2)
+			return await app.send_edit(m,"This bot can't be used in inline mode.", delme=2)
 
 		if result:
-			try:
-				await app.send_inline_bot_result(
-					m.chat.id, 
-					query_id=result.query_id, 
-					result_id=result.results[0].id, 
-					disable_notification=True, 
-					hide_via=True
-					)
-				await m.delete()
-			except Exception as e:
-				await error(m, e)
-		else:
-			await send_edit(
-				m, 
-				"Please try again later !",
-				delme=2,
-				mono=True
+			await app.send_inline_bot_result(
+				m.chat.id, 
+				query_id=result.query_id, 
+				result_id=result.results[0].id, 
+				disable_notification=True, 
+				hide_via=True
 				)
+			await m.delete()
+		else:
+			await app.send_edit(m, "Please try again later !", delme=2, mono=True)
 	except Exception as e:
-		await error(m, e)
+		await app.error(m, e)
 
