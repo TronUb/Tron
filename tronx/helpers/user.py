4e730e2dc71a746f94c27eecae948dfeda17a192
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2022-01-24 08:10:49 +0530

    Create user.py

--- tronx/helpers/user.py
+++ tronx/helpers/user.py
@@ -1,22 +1,17 @@
-from tronx.database.postgres import dv_sql as dv
-
-
-
-
 class User(object):
 	# instant change of names through database vars
 	def UserName(self):
-		var = dv.getdv("USER_NAME")
+		var = self.getdv("USER_NAME")
 		one = var if bool(var) is True else self.USER_NAME
-		two = one if one else self.REAL_USER_NAME
+		two = one if one else self.name
 		return two if two else None
 
 
 	# username of bot owner
 	def UserUsername(self):
-		var = dv.getdv("USER_USERNAME")
+		var = self.getdv("USER_USERNAME")
 		one = var if bool(var) is True else self.USER_USERNAME
-		two = one if one else self.REAL_USER_USERNAME
+		two = one if one else self.username
 		return two if two else None
 
 
@@ -27,13 +22,13 @@ class User(object):
 
 	# telegram id of bot owner
 	def UserId(self):
-		var = dv.getdv("USER_ID")
+		var = self.getdv("USER_ID")
 		one = var if bool(var) is True else self.USER_ID
-		two = one if one else self.REAL_USER_ID
+		two = one if one else self.id
 		return two if two else None
 
 
 	# dc id of bot owner
 	def UserDc(self):
-		data = self.USER_DC if self.USER_DC else self.REAL_USER_DC
+		data = self.USER_DC if self.USER_DC else self.dc_id
 		return data if data else None
