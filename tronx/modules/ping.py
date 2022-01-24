52652d8bba7e873a2ab8ee470f16edd9200863ec
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2022-01-24 21:27:05 +0530

    Update ping.py

--- tronx/modules/ping.py
+++ tronx/modules/ping.py
@@ -9,29 +9,16 @@ from datetime import datetime
 from pyrogram import filters
 from pyrogram.types import Message, User
 
-from tronx import (
-	app, 
-	CMD_HELP, 
-	StartTime, 
-	USER_NAME, 
-	USER_ID,
-	Config,
-	PREFIX
-	)
+from tronx import app
 
 from tronx.helpers import (
-	error,
 	gen,
-	send_edit,
-	mymention,
-	long,
 )
 
 
 
 
-
-CMD_HELP.update(
+app.CMD_HELP.update(
 	{"ping" : (
 		"ping",
 		{
@@ -68,37 +55,37 @@ pings.clear()
 
 @app.on_message(gen(["ping", "pong"], allow_channel=True))
 async def pingme(_, m: Message):
-	if long(m) == 1:
+	if app.long(m) == 1:
 		start = datetime.now()
-		await send_edit(m, ". . .", mono=True)
+		await app.send_edit(m, ". . .", mono=True)
 		end = datetime.now()
 		m_s = (end - start).microseconds / 1000
-		await send_edit(
+		await app.send_edit(
 			m, 
-			f"**Pöng !**\n`{m_s} ms`\n⧑ {mymention()}", 
+			f"**Pöng !**\n`{m_s} ms`\n⧑ {app.mymention()}", 
 			disable_web_page_preview=True
 			)
-	elif long(m) == 2:
+	elif app.long(m) == 2:
 		count = m.command[1]
 		text = int(m.command[1])
 		if text == 1:
-			return await send_edit(m, "If you need one ping use only `.ping`", delme=2)
+			return await app.send_edit(m, "If you need one ping use only `.ping`", delme=2)
 
 		elif text == 0:
-			return await send_edit(m, "try a greater number like 2.", delme=2, mono=True)
+			return await app.send_edit(m, "try a greater number like 2.", delme=2, mono=True)
 
 		else:
 			try:
 				num = int(count) + 1
 				for x in range(1, num):
 					await infinite(m)
-					await send_edit(m, ". . .", mono=True)
+					await app.send_edit(m, ". . .", mono=True)
 					time.sleep(0.50)
-				await send_edit(m, "".join(pings))
+				await app.send_edit(m, "".join(pings))
 			except Exception as e:
-				await error(m, e)
+				await app.error(m, e)
 	else:
-		return await send_edit(m, "Something went wrong in ping module.", delme=2)
+		return await app.send_edit(m, "Something went wrong in ping module.", delme=2)
 
 
 
@@ -106,13 +93,10 @@ async def pingme(_, m: Message):
 # function to create lots of pings
 async def infinite(m: Message):
 	start = datetime.now()
-	mid = await send_edit(
-		m, 
-		random.choice(data)
-		)
+	mid = await app.send_edit(m, random.choice(data))
 	end = datetime.now()
 	ms = (end - start).microseconds / 1000
-	msg = f"Pöng !\n{ms} ms\n⧑ {mymention()}\n\n"
+	msg = f"Pöng !\n{ms} ms\n⧑ {app.mymention()}\n\n"
 	pings.append(msg)
 
 
