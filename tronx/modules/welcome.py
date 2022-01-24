0a985763f4e7489ee8f8c8a87ae776d142bdc1da
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2022-01-24 23:15:31 +0530

    Update welcome.py

--- tronx/modules/welcome.py
+++ tronx/modules/welcome.py
@@ -5,17 +5,9 @@ from pyrogram import filters
 
 from tronx import app
 
-from tronx.database.postgres import welcome_sql as dw   
-
 from tronx.helpers import (
-	send_edit,
-	error,
 	gen,
-	long,
-	private,
-	get_file_id
 )
-
 
 
 
@@ -37,7 +29,7 @@ app.CMD_HELP.update(
 
 @app.on_message(filters.new_chat_members & filters.group)
 async def send_welcome(_, m: Message):
-	chat = dw.get_welcome(str(m.chat.id))
+	chat = app.get_welcome(str(m.chat.id))
 	if bool(chat) is True:
 		if chat["file_id"] is None:
 			return
@@ -65,64 +57,64 @@ async def send_welcome(_, m: Message):
 				reply_to_message_id=m.message_id
 			)
 	except Exception as e:
-		await error(m, e)
+		await app.error(m, e)
 
 
 
 
 @app.on_message(gen(["setwelcome", "setwc"]))
 async def save_welcome(_, m: Message):
-	await private(m)
-	await send_edit(m, "Setting this media as a welcome message . . .", mono=True)
+	await app.private(m)
+	await app.send_edit(m, "Setting this media as a welcome message . . .", mono=True)
 	reply = m.reply_to_message
 	if reply:
 		try:
-			fall = get_file_id(m)
+			fall = app.get_file_id(m)
 			file_id = fall["data"] if fall["data"] else None
 			caption = fall["caption"] if fall["caption"] else None
 
 			if bool(reply.media) is True:
 				if caption:
-					dw.set_welcome(str(m.chat.id), "#" + file_id, caption)
+					app.set_welcome(str(m.chat.id), "#" + file_id, caption)
 				else:
-					dw.set_welcome(str(m.chat.id), "#" + file_id)
-				await send_edit(m, "Added this media to welcome message . . .", delme=2, mono=True)
+					app.set_welcome(str(m.chat.id), "#" + file_id)
+				await app.send_edit(m, "Added this media to welcome message . . .", delme=2, mono=True)
 			elif bool(reply.media) is False:
-				dw.set_welcome(str(m.chat.id), reply.text)
-				await send_edit(m, "Added this text to welcome message . . .", delme=2, mono=True)
+				app.set_welcome(str(m.chat.id), reply.text)
+				await app.send_edit(m, "Added this text to welcome message . . .", delme=2, mono=True)
 		except Exception as e:
-			await error(m, e)
+			await app.error(m, e)
 			print(e)
 	else:
-		await send_edit(m, "Please reply to some media or text to set welcome message . . .", delme=2, mono=True)      
+		await app.send_edit(m, "Please reply to some media or text to set welcome message . . .", delme=2, mono=True)      
 
 
 
 
 @app.on_message(gen(["delwelcome", "delwc"]))
 async def delete_welcome(_, m: Message):
-	await private(m)
+	await app.private(m)
 	try:
-		await send_edit(m, "Checking welcome message for this group . . .", mono=True)
-		dw.del_welcome(str(m.chat.id))
-		await send_edit(m, "Successfully deleted welcome message for this chat . . .", delme=2, mono=True)
+		await app.send_edit(m, "Checking welcome message for this group . . .", mono=True)
+		app.del_welcome(str(m.chat.id))
+		await app.send_edit(m, "Successfully deleted welcome message for this chat . . .", delme=2, mono=True)
 	except Exception as e:
-		await error(m, e)
+		await app.error(m, e)
 
 
 
 
 @app.on_message(gen(["getwelcome", "getwc"]))
 async def delete_welcome(_, m: Message):
-	await private(m)
+	await app.private(m)
 	try:
-		await send_edit(m, "Getting welcome message of this group . . .")
-		data = dw.get_welcome(str(m.chat.id))
+		await app.send_edit(m, "Getting welcome message of this group . . .")
+		data = app.get_welcome(str(m.chat.id))
 		text = data["file_id"]
 		cap = data["caption"]
 
 		if text is None and cap is None :
-			await send_edit(m, "No welcome message was assigned to this group.", mono=True, delme=3)
+			await app.send_edit(m, "No welcome message was assigned to this group.", mono=True, delme=3)
 		elif text is not None and cap is None:
 			if text.startswith("#"):
 				await app.send_cached_media(
@@ -132,7 +124,7 @@ async def delete_welcome(_, m: Message):
 					)
 				await m.delete()
 			else:
-				await send_edit(m, text, mono=True)
+				await app.send_edit(m, text, mono=True)
 		elif text is not None and cap is not None:
 			if text.startswith("#"):
 				await app.send_cached_media(
@@ -143,7 +135,7 @@ async def delete_welcome(_, m: Message):
 					)
 				await m.delete()
 	except Exception as e:
-		await error(m, e)
+		await app.error(m, e)
 
 
 
