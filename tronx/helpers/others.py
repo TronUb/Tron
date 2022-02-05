3a2c9336a52129964cf890bed20a416bfe75748e
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2022-02-05 22:38:46 +0530

    Update others.py

--- tronx/helpers/others.py
+++ tronx/helpers/others.py
@@ -1,12 +1,12 @@
 class Others(object):
 	def NoLoad(self):
 		noloadvar = self.getdv("NO_LOAD")
-		data_list = [int(x) for x in noloadvar.split()] if self.is_int((x for x in noloadvar.split())) else False  
+		data_list = [int(x) for x in noloadvar.split()] if noloadvar else False  
 		return data_list or self.NO_LOAD or [] 
 
 
 	def SudoUsers(self):
 		sudovar = self.getdv("SUDO_USERS")
-		data_list = [int(x) for x in sudovar.split()] if self.is_int((x for x in sudovar.split())) else False  
+		data_list = [int(x) for x in sudovar.split()] if sudovar else False  
 		return data_list or self.SUDO_USERS or [] 
 
