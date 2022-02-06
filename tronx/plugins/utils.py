cc5d3ae5489241cd2ee82adafbdd8f3aa975add7
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2022-02-06 10:16:29 +0530

    Update utils.py

--- tronx/plugins/utils.py
+++ tronx/plugins/utils.py
@@ -1,31 +1,31 @@
+from tronx import app
+
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-from tronx import app
 
 
 
 
 
 @app.bot.on_message(filters.command("id"))
-async def id_of_user(_, m):
+async def chat_user_id(_, m):
 	reply = m.reply_to_message
 	if not reply:
 		await app.bot.send_message(
 			m.chat.id,
-			f"**YOUR ID:** `{m.from_user.id}`\n**CHAT ID:** `{m.chat.id}`"
+			f"**{m.from_user.first_name}:** `{m.from_user.id}`\n**{m.chat.title}:** `{m.chat.id}`"
 		)
 	elif reply:
 		await app.bot.send_message(
 			m.chat.id,
-			f"**REPLIED USER ID:** `{m.from_user.id}`\n**CHAT ID:** `{m.chat.id}`\n**REPLIED ID:** `{reply.from_user.id}`"
+			f"**{m.from_user.first_name}:** `{m.from_user.id}`\n**{m.chat.title}:** `{m.chat.id}`\n**{reply.from_user.first_name}:** `{reply.from_user.id}`"
 		)
 
 
 
 
 @app.bot.on_message(filters.command("quote"))
-async def get_anime_quotes(_, m):
+async def bot_anime_quotes(_, m):
 	await app.bot.send_message(
 		m.chat.id,
 		app.quote(),
