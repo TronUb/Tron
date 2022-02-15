559e933ddae7290cc2259a45491d5c0e246426fa
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2022-02-15 20:06:36 +0530

    Update sudo.py

--- tronx/modules/sudo.py
+++ tronx/modules/sudo.py
@@ -21,3 +21,29 @@ async def add_sudo(_, m: Message):
 	await app.send_edit(m, f"{reply.from_user.mention()} `has been added to sudo.`", delme=4)
 
 
+
+
+@app.on_message(gen("getsudo"))
+async def get_sudo(_, m: Message):
+	sudo_list = [x for x app.getdv("SUDO_USERS").split()]
+	await app.send_edit(m, f"**Available Sudo id:**\n\n{'\n'.join(sudo_list)}")
+
+
+
+
+@app.on_message(gen("delsudo"))
+async def get_sudo(_, m: Message):
+	reply = m.reply_to_message
+	user_id = str(reply.from_user.id)
+	if not reply:
+		return await app.send_edit(m, "Reply to a user to add him in sudo list", mono=True, delme=4)  
+
+	sudo_list = [x for x app.getdv("SUDO_USERS").split()]
+	if user_id in sudo_list:
+		sudo_list.remove(user_id)
+	else:
+		return await app.send_edit(m, "This user is not in sudo list", mono=True, delme=4)
+
+	await app.send_edit(m, f"{reply.from_user.mention()} `has been removed from sudo list`", delme=4)
+
+
