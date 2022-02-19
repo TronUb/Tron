78cca8cd1e14b23975020773ee71262687789cc5
beastzx18 <vs423502@gmail.com>
2022-02-19 20:21:13 +0530

    test

--- tronx/helpers/filters.py
+++ tronx/helpers/filters.py
@@ -111,10 +111,18 @@ def gen(
 		if not text:
 			return False
 
+		if text and len(text) > 4096:
+			try:
+				await app.send_edit(message, "The length of text exceeds 4096 characters.", mono=True, delme=4)
+			except Exception:
+				pass
+			return False
+
 		# works for you & sudo | only for you
 		if allow_sudo:
 			if message.from_user and not (message.from_user.is_self or message.from_user.id in client.SudoUsers()):
 				return False
+
 		elif not allow_sudo:
 			if message.from_user and not message.from_user.is_self:
 				return False
