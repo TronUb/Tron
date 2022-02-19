a52d5242b974de2585f395a0ecf583d74bea5c29
beastzx18 <vs423502@gmail.com>
2022-02-19 16:36:45 +0530

    update

--- tronx/modules/reddit.py
+++ tronx/modules/reddit.py
@@ -45,11 +45,11 @@ async def reddit_image(_, m: Message):
 				pic_url = ch["data"].get("url_overridden_by_dest")
 				if pic_url:
 					file_name = pic_url.split("/")[-1]
-						if not "." in file_name:
-							continue
-						with open(file_name, "wb") as f_out:
-							c = requests.get(pic_url, headers=headers).content
-							f_out.write(c)
+					if not "." in file_name:
+						continue
+					with open(file_name, "wb") as f_out:
+						c = requests.get(pic_url, headers=headers).content
+						f_out.write(c)
 
 			photos = []
 			photos.clear()
