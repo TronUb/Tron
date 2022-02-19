8bb89d1c606846840730ef186cfed9907e419499
beastzx18 <vs423502@gmail.com>
2022-02-19 18:00:07 +0530

    updating reddit

--- tronx/modules/reddit.py
+++ tronx/modules/reddit.py
@@ -17,7 +17,7 @@ app.CMD_HELP.update(
 	{"reddit" : (
 		"reddit",
 		{
-		"r [query]" : "Get link of queries related reddit results."
+		"r [query]" : "Get reddit images (limit = 1)"
 		}
 		)
 	}
@@ -26,21 +26,21 @@ app.CMD_HELP.update(
 
 
 
-@app.on_message(gen(["r", "reddit"]))
+@app.on_message(gen(["r", "reddit"], allow_sudo=True))
 async def reddit_image(_, m: Message):
 	if app.long(m) == 1:
-		return await app.send_edit(m, "Please give some query to search on reddit ...", delme=2)
+		return await app.send_edit(m, "Please give me some query to search on reddit.", delme=2)
+
+	elif app.textlen(m) > 4096:
+		return await app.send_edit(m, "Too long query, only 4096 characters are allowed !", mono=True, delme=2)
 
 	elif app.long(m) > 1:
 		try:
-			text = m.text.split(None, 1)[1]
-			url = f"https://old.reddit.com/r/{text}.json"
-
-			headers = {
-				"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:87.0) Gecko/20100101 Firefox/87.0"
-			}
+			m = await app.send_edit(m, "Getting reddit images . . .", mono=True)
+			query = m.text.split(None, 1)[1]
+			url = f"https://old.reddit.com/r/{query}.json"
+			headers = {"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:87.0) Gecko/20100101 Firefox/87.0"}   
 			data = requests.get(url, headers=headers).json()
-
 
 			for ch in data["data"]["children"]:
 				pic_url = ch["data"].get("url_overridden_by_dest")
@@ -49,29 +49,20 @@ async def reddit_image(_, m: Message):
 					if not "." in file_name:
 						continue
 					with open(file_name, "wb") as f_out:
-						c = requests.get(pic_url, headers=headers).content
-						f_out.write(c)
+						kate = requests.get(pic_url, headers=headers).content
+						f_out.write(kate)
 
-			photos = []
-			photos.clear()
-
-			for x in os.listdir("."):
-				if x.endswith(".jpg") or x.endswith(".png"):
-					photos.append(x)
+			photos = [x for x in os.listdir() if x.endswith(".jpg") or x.endswith(".png") or x.endswith(".gif") else ""]
 
 			await app.send_photo(m.chat.id, random.choice(photos))
 
 			# finally remove
 			for x in os.listdir("."):
-				if x.endswith(".jpg") or x.endswith(".png"):
+				if x.endswith(".jpg") or x.endswith(".png"): or x.endswith(".gif"):
 					os.remove(x)
+
+			await m.delete()
 
 		except Exception as e:
 			await app.error(m, e)
-
-	elif app.textlen(m) > 4096:
-		return await app.send_edit(m, "Too long query, only 4096 characters are allowed !", delme=2)
-	else:
-		await app.send_edit(m, "Try again later !", delme=2)
-
 
