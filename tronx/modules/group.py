4d41ad9bc96135543e2507702389f2f695c26899
vs423502@gmail.com <vs423502@gmail.com>
2022-03-14 17:35:08 +0530

    minor

--- tronx/modules/group.py
+++ tronx/modules/group.py
@@ -27,22 +27,22 @@ app.CMD_HELP.update(
 @app.on_message(gen(["bgroup", "bgp"], allow =["sudo"]))
 async def basicgroup_handler(_, m: Message):
 	if app.long(m) == 1:
-		return await app.send_edit(m, f"`Usage: {app.PREFIX}bgroup mygroupname`", delme=4)
+		return await app.send_edit(m, f"Usage: `{app.PREFIX}bgroup mygroupname`", delme=4)
 	elif app.long(m) > 1:
 		grpname = m.text.split(None, 1)[1]
 		about = ""
 	elif app.long(m) > 2:
 		grpname = m.text.split(None, 1)[1]
-		about = m.text.split(None, 2)[2]
+		users = m.text.split(None, 2)[2].split()
 	else:
 		grpname = False
-		about = ""
+		users = "@TheRealPhoenixBot" # required
 
 	try:
 		if grpname:
 			m = await app.send_edit(m, f"Creating a new basic group: `{grpname}`")
-			group = await app.create_group(title=f"{grpname}", description=about)
-			await app.send_edit(m, f"**Created a new super Group:** [{grpname}]({(await app.get_chat(group.id)).invite_link})")
+			group = await app.create_group(title=f"{grpname}", users=users)
+			await app.send_edit(m, f"**Created a new basic group:** [{grpname}]({(await app.get_chat(group.id)).invite_link})")
 		else:
 			await app.send_edit(m, "No group name is provided.", text_type=["mono"], delme=4)
 	except Exception as e:
@@ -69,7 +69,7 @@ async def supergroup_handler(_, m: Message):
 		if grpname:
 			m = await app.send_edit(m, f"Creating a new super group: `{grpname}`")
 			group = await app.create_supergroup(title=f"{grpname}", description=about)
-			await app.send_edit(m, f"**Created a new super Group:** [{grpname}]({(await app.get_chat(group.id)).invite_link})")
+			await app.send_edit(m, f"**Created a new super group:** [{grpname}]({(await app.get_chat(group.id)).invite_link})")
 		else:
 			await app.send_edit(m, "No group name is provided.", text_type=["mono"], delme=4)
 	except Exception as e:
@@ -99,7 +99,7 @@ async def unreadchat_handler(_, m: Message):
 @app.on_message(gen("channel", allow =["sudo"]))
 async def channel_handler(_, m: Message):
 	if app.long(m) == 1:
-		return await app.send_edit(m, f"`Usage: {app.PREFIX}channel [channel name]`", delme=4)
+		return await app.send_edit(m, f"Usage: `{app.PREFIX}channel [channel name]`", delme=4)
 
 	elif app.long(m) > 1:
 		chname = m.text.split(None, 1)[1]
@@ -113,7 +113,7 @@ async def channel_handler(_, m: Message):
 			m = await app.send_edit(m, f"Creating your channel: `{chname}`")
 			response = await app.create_channel(title=f"{chname}", description=about)
 			if response:
-				await app.send_edit(m, f"**Created channel:** [{chname}]({(app.get_chat(response.id)).invite_link})", disable_web_page_preview=True)
+				await app.send_edit(m, f"**Created a new channel:** [{chname}]({(await app.get_chat(response.id)).invite_link})", disable_web_page_preview=True)
 			else:
 				await app.send_edit(m, "Couldn't create a channel.")
 	except Exception as e:
