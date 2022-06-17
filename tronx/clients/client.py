bf46646b048b9aba926b35536ed659ba60114ec9
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2022-06-17 23:38:25 +0530

    Beta (#107)
    * Update on_message.py
    
    * Update on_callback.py
    
    * Update on_inline.py
    
    * Update client.py
    
    * Update client.py
    
    updating according to new pyrogram changes
    
    * Update client.py
    
    * Update rawfunctions.py
    
    * Update rawfunctions.py
    
    * Update rawfunctions.py
    
    * Update rawfunctions.py
    
    * Update rawfunctions.py

--- tronx/clients/client.py
+++ tronx/clients/client.py
@@ -1,16 +1,21 @@
+import random
+import string
 from pyrogram import Client
 from .utils import Utils
 
 
+# temp -
+random_name =  ''.join(random.choices(string.ascii_uppercase + string.digits, k = 7))
 
 
 class SuperClient(Utils, Client):
 	""" Userbot (tron) """
 	def __init__(self):
 		super().__init__(
-		session_name=self.SESSION,
+		name=random_name,
 		api_id=self.API_ID,
 		api_hash=self.API_HASH,
+		session_string=self.SESSION,
 		workers=self.WORKERS,
 		)
 		self.start()
@@ -29,7 +34,7 @@ class SuperClient(Utils, Client):
 		""" Assistant (Nora) """
 		def __init__(self):
 			super().__init__(
-			session_name="Nora",
+			name="Nora",
 			api_id=self.API_ID,
 			api_hash=self.API_HASH,
 			bot_token=self.TOKEN,
