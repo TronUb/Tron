5d65957fb47cab57aa10e257b879e63920861fea
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2022-06-17 23:06:39 +0530

    Update rawfunctions.py

--- tronx/helpers/functions/rawfunctions.py
+++ tronx/helpers/functions/rawfunctions.py
@@ -214,7 +214,7 @@ class RawFunctions(object):
 		self,
 		message: Message, 
 		text: str, 
-		parse_mode="", 
+		parse_mode="default", 
 		disable_web_page_preview=False,
 		delme : int=0,
 		text_type: list=[],
