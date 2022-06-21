fd6c96c8831a27ad779614479f2c0fd275037912
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2022-06-21 21:40:13 +0530

    Update reddit.py

--- tronx/modules/reddit.py
+++ tronx/modules/reddit.py
@@ -4,11 +4,7 @@ import random
 import requests
 from pyrogram.types import Message
 
-from tronx import app
-
-from tronx.helpers import (
-	gen,
-)
+from tronx import app, gen
 
 
 
@@ -28,16 +24,16 @@ app.CMD_HELP.update(
 
 @app.on_message(gen(["r", "reddit"], allow = ["sudo", "channel"]))
 async def reddit_handler(_, m: Message):
-	if app.long(m) == 1:
-		return await app.send_edit(m, "Please give me some query to search on reddit.", delme=2)
+	if app.long() == 1:
+		return await app.send_edit("Please give me some query to search on reddit.", delme=2)
 
-	elif app.textlen(m) > 4096:
-		return await app.send_edit(m, "Too long query, only 4096 characters are allowed !", text_type=["mono"], delme=2)
+	elif app.textlen() > 4096:
+		return await app.send_edit("Too long query, only 4096 characters are allowed !", text_type=["mono"], delme=2)
 
-	elif app.long(m) > 1:
+	elif app.long() > 1:
 		try:
 			query = m.text.split(None, 1)[1]
-			m = await app.send_edit(m, "Getting reddit images . . .", text_type=["mono"])
+			await app.send_edit("Getting reddit images . . .", text_type=["mono"])
 			url = f"https://old.reddit.com/r/{query}.json"
 			headers = {"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:87.0) Gecko/20100101 Firefox/87.0"}   
 			data = requests.get(url, headers=headers).json()
@@ -63,5 +59,5 @@ async def reddit_handler(_, m: Message):
 			await m.delete()
 
 		except Exception as e:
-			await app.error(m, e)
+			await app.error(e)
 
