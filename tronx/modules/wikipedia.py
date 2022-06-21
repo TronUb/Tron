cf1ed4cc7029d26493541d3862b307c88c42bced
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2022-06-21 22:11:17 +0530

    Update wikipedia.py

--- tronx/modules/wikipedia.py
+++ tronx/modules/wikipedia.py
@@ -24,24 +24,24 @@ app.CMD_HELP.update(
 
 @app.on_message(gen("wiki", allow = ["sudo", "channel"]))
 async def wikipedia_handler(_, m: Message):
-	if app.long(m) == 1:
-		await app.send_edit(m, "Give me some query to search on wikipedia . . .", text_type=["mono"], delme=True)
+	if app.long() == 1:
+		await app.send_edit("Give me some query to search on wikipedia . . .", text_type=["mono"], delme=True)
 
-	elif app.long(m) > 1 and app.long(m) < 4096:
+	elif app.long() > 1 and app.long() < 4096:
 		try:
 			obj = wikipediaapi.Wikipedia("en")
 			text = m.text.split(None, 1)[1]
 			result = obj.page(text)
-			await app.send_edit(m, f"Searching for: __{text}__ . . .", text_type=["mono"])
+			await app.send_edit(f"Searching for: __{text}__ . . .", text_type=["mono"])
 			if result:
 				giveresult = result.summary
 				if len(giveresult) <= 4096:
-					await app.send_edit(m, f"**Results for:** `{text}`\n\n```{giveresult}```")
+					await app.send_edit(f"**Results for:** `{text}`\n\n```{giveresult}```")
 				else:
-					await app.send_edit(m, f"**Results for:** `{text}`\n\n```{giveresult[:4095]}```")
+					await app.send_edit(f"**Results for:** `{text}`\n\n```{giveresult[:4095]}```")
 			else:
-				await app.send_edit(m, "No results found !", delme=2, text_type=["mono"])
+				await app.send_edit("No results found !", delme=2, text_type=["mono"])
 		except Exception as e:
-			await app.error(m, e)
+			await app.error(e)
 	else:
-		await app.send(m, "Something went wrong !", text_type=["mono"], delme=3)
+		await app.send("Something went wrong !", text_type=["mono"], delme=3)
