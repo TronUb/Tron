7dd0d88c443c860416b59ed96caa175df09d7f20
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2022-06-21 21:50:36 +0530

    Update sudo.py

--- tronx/modules/sudo.py
+++ tronx/modules/sudo.py
@@ -1,6 +1,5 @@
 from pyrogram.types import Message
-from tronx.helpers import gen
-from tronx import app
+from tronx import app, gen
 
 
 
@@ -34,7 +33,7 @@ async def addsudo_handler(_, m: Message):
 		all_sudo = [str(reply.from_user.id)]
 
 	app.setdv("SUDO_USERS", " ".join(list(set(all_sudo)))) # rem duplicates
-	await app.send_edit(m, f"{reply.from_user.mention()} `has been added to sudo.`", delme=4)
+	await app.send_edit(f"{reply.from_user.mention()} `has been added to sudo.`", delme=4)
 
 
 
@@ -42,7 +41,7 @@ async def addsudo_handler(_, m: Message):
 @app.on_message(gen("listsudo", allow = ["sudo"]))
 async def getsudo_handler(_, m: Message):
 	sudo_list = [x for x in app.getdv("SUDO_USERS").split()]
-	await app.send_edit(m, "**Available Sudo id:**\n\n" + "\n".join(sudo_list))
+	await app.send_edit("**Available Sudo id:**\n\n" + "\n".join(sudo_list))
 
 
 
@@ -52,15 +51,15 @@ async def delsudo_handler(_, m: Message):
 	reply = m.reply_to_message
 	user_id = str(reply.from_user.id)
 	if not reply:
-		return await app.send_edit(m, "Reply to a user to add him in sudo list", text_type=["mono"], delme=4)  
+		return await app.send_edit("Reply to a user to add him in sudo list", text_type=["mono"], delme=4)  
 
 	sudo_list = [x for x in app.getdv("SUDO_USERS").split()]
 	if user_id in sudo_list:
 		sudo_list.remove(user_id)
 		app.setdv("SUDO_USERS", " ".join(sudo_list))
 	else:
-		return await app.send_edit(m, "This user is not in sudo list", text_type=["mono"], delme=4)
+		return await app.send_edit("This user is not in sudo list", text_type=["mono"], delme=4)
 
-	await app.send_edit(m, f"{reply.from_user.mention()} `has been removed from sudo list`", delme=4)
+	await app.send_edit(f"{reply.from_user.mention()} `has been removed from sudo list`", delme=4)
 
 
