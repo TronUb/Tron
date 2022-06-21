0ced37df4dab1fd0bce67e4a9a0fb79daa13647f
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2022-06-21 21:58:48 +0530

    Update translate.py

--- tronx/modules/translate.py
+++ tronx/modules/translate.py
@@ -30,36 +30,36 @@ async def translate_handler(_, m: Message):
 	oldmsg = m
 
 	try:
-		lang = cmd[1] if app.long(m) > 1 else "en"
+		lang = cmd[1] if app.long() > 1 else "en"
 
-		m = await app.send_edit(m, f"**Translating in** `{lang}` . . .")
+		await app.send_edit(f"**Translating in** `{lang}` . . .")
 
 		languages = list((gtl.get_supported_languages(as_dict=True)).values())
 
 		if not lang in languages:
-			return await app.send_edit(m, "Bot doesn't support this language code, please try different one.", text_type=["mono"], delme=4)
+			return await app.send_edit("Bot doesn't support this language code, please try different one.", text_type=["mono"], delme=4)
 
 		if (reply and reply.text):
-			tdata = await translate(m, lang=lang, text=reply.text)
-			await app.send_edit(m, f"**Translated to:** `{lang}`\n\n**Text: **`{tdata}`")
+			tdata = await translate(lang=lang, text=reply.text)
+			await app.send_edit(f"**Translated to:** `{lang}`\n\n**Text: **`{tdata}`")
 
 		elif not reply and app.textlen(oldmsg) <= 4096:
-			if app.long(m) <= 2:
-				return await app.send_edit(m, "Give me the language code with text to translate.", text_type=["mono"], delme=4)
+			if app.long() <= 2:
+				return await app.send_edit("Give me the language code with text to translate.", text_type=["mono"], delme=4)
 
 			text = m.text.split(None, 2)[2]
-			tdata = await translate(m, lang=lang, text=text)
-			await app.send_edit(m, f"**Translated to:** `{lang}`\n\n**Text:** `{tdata}`")
+			tdata = await translate(lang=lang, text=text)
+			await app.send_edit(f"**Translated to:** `{lang}`\n\n**Text:** `{tdata}`")
 		else:
-			await app.send_edit(m, "Something went wrong, please try again later !", text_type=["mono"], delme=4)
+			await app.send_edit("Something went wrong, please try again later !", text_type=["mono"], delme=4)
 
 	except Exception as e:
-		await app.error(m, e)
+		await app.error(e)
 
 
 
 
-async def translate(m: Message, lang, text):
+async def translate(lang, text):
 	tr = GoogleTranslator(source="auto", target=lang)
 	return tr.translate(text)
 
@@ -75,5 +75,5 @@ async def translatelang_handler(_, m):
 	for keys, values in zip(langs_list.values(), langs_list.keys()):
 		data.append(f"`{keys}` : `{values}`")
 
-	await app.send_edit(m, "**SUPPORTED LANGUAGES:**\n\n" + "\n".join(data))
+	await app.send_edit("**SUPPORTED LANGUAGES:**\n\n" + "\n".join(data))
 		
