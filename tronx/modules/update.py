79e733ce7759a5c771f78142fbf57fd70c86ce1a
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2022-06-22 16:43:20 +0530

    Update update.py

--- tronx/modules/update.py
+++ tronx/modules/update.py
@@ -73,80 +73,82 @@ async def install_requirements():
 
 @app.on_message(gen("update", allow = ["sudo", "channel"]))
 async def update_handler(_, m):
-	cmd = False
-	errtext = "Some problem occurred:\n\n"
-	await app.send_edit("Checking for updates, please wait . . .", text_type=["mono"])
-
-	if app.long() > 1 and m.command[1] == "now":
-		cmd = m.command
-	elif app.long() > 1 and m.command[1] != "now":
-		return await app.send_edit("Use `now` after update command")
-	elif app.long() == 1:
-		if await gen_chlog() == -1:
-			return
-
-	await app.send_edit("Found update, updating . . .", text_type=["mono"])
 	try:
-		repo = Repo()
-	except NoSuchPathError as e:
-		await app.send_edit(f"{errtext}`{e}`")
-		return repo.__del__()
+		cmd = False
+		errtext = "Some problem occurred:\n\n"
+		await app.send_edit("Checking for updates, please wait . . .", text_type=["mono"])
 
-	except GitCommandError as e:
-		await app.send_edit(f"{errtext}`{e}`")
-		return repo.__del__()
+		if app.long() > 1 and m.command[1] == "now":
+			cmd = m.command
+		elif app.long() > 1 and m.command[1] != "now":
+			return await app.send_edit("Use `now` after update command")
+		elif app.long() == 1:
+			if await gen_chlog() == -1:
+				return
 
-	except InvalidGitRepositoryError as e:
-		repo = Repo.init()
-		origin = repo.create_remote("upstream", TRON_REPO)
-		origin.fetch()
-		repo.create_head("master", origin.refs.master)
-		repo.heads.master.set_tracking_branch(origin.refs.master)
-		repo.heads.master.checkout(True)
-	ACTIVE_BRANCH = repo.active_branch.name
-	if ACTIVE_BRANCH != "master":
-		await app.send_edit(f"**[ UPDATER ]:** You are on [ {ACTIVE_BRANCH} ]\n\nPlease change to `master` branch.`")
-		return repo.__del__()
-
-	try:
-		repo.create_remote("upstream", TRON_REPO)
-	except BaseException:
-		pass
-
-	ups_rem = repo.remote("upstream")
-	ups_rem.fetch(ACTIVE_BRANCH)
-
-	if app.HEROKU_API_KEY and app.HEROKU_APP_NAME:
-		heroku_conn = heroku3.from_key(app.HEROKU_API_KEY)
-		heroku_app = heroku_conn.apps()[app.HEROKU_APP_NAME]
-
-		await app.send_edit("Userbot update in progress, please wait for few minutes . . .", text_type=["mono"])
-		ups_rem.fetch(ACTIVE_BRANCH)
-		repo.git.reset("--hard", "FETCH_HEAD")
-		heroku_git_url = heroku_app.git_url.replace("https://", "https://api:" + app.HEROKU_API_KEY + "@")
-
-		if "heroku" in repo.remotes:
-			remote = repo.remote("heroku")
-			remote.set_url(heroku_git_url)
-		else:
-			remote = repo.create_remote("heroku", heroku_git_url)
-
+		await app.send_edit("Found update, updating . . .", text_type=["mono"])
 		try:
-			remote.push(refspec=f"HEAD:refs/heads/{ACTIVE_BRANCH}", force=True)
+			repo = Repo()
+		except NoSuchPathError as e:
+			await app.send_edit(f"{errtext}`{e}`")
+			return repo.__del__()
+
 		except GitCommandError as e:
-			print(e)
-			app.log.error(e)
-			return
+			await app.send_edit(f"{errtext}`{e}`")
+			return repo.__del__()
 
-		await app.send_edit("Successfully Updated, initialing . . .", text_type=["mono"], delme=8)
+		except InvalidGitRepositoryError as e:
+			repo = Repo.init()
+			origin = repo.create_remote("upstream", TRON_REPO)
+			origin.fetch()
+			repo.create_head("master", origin.refs.master)
+			repo.heads.master.set_tracking_branch(origin.refs.master)
+			repo.heads.master.checkout(True)
+		ACTIVE_BRANCH = repo.active_branch.name
+		if ACTIVE_BRANCH != "master":
+			await app.send_edit(f"**[ UPDATER ]:** You are on [ {ACTIVE_BRANCH} ]\n\nPlease change to `master` branch.`")
+			return repo.__del__()
 
-	else:
 		try:
-			ups_rem.pull(ACTIVE_BRANCH)
-		except GitCommandError:
+			repo.create_remote("upstream", TRON_REPO)
+		except BaseException:
+			pass
+
+		ups_rem = repo.remote("upstream")
+		ups_rem.fetch(ACTIVE_BRANCH)
+
+		if app.HEROKU_API_KEY and app.HEROKU_APP_NAME:
+			heroku_conn = heroku3.from_key(app.HEROKU_API_KEY)
+			heroku_app = heroku_conn.apps()[app.HEROKU_APP_NAME]
+
+			await app.send_edit("Userbot update in progress, please wait for few minutes . . .", text_type=["mono"])
+			ups_rem.fetch(ACTIVE_BRANCH)
 			repo.git.reset("--hard", "FETCH_HEAD")
+			heroku_git_url = heroku_app.git_url.replace("https://", "https://api:" + app.HEROKU_API_KEY + "@")
 
-		await install_requirements()
-		await app.send_edit("Successfully updated Userbot!\nBot is restarting . . .", text_type=["mono"], delme=8)
+			if "heroku" in repo.remotes:
+				remote = repo.remote("heroku")
+				remote.set_url(heroku_git_url)
+			else:
+				remote = repo.create_remote("heroku", heroku_git_url)
 
+			try:
+				remote.push(refspec=f"HEAD:refs/heads/{ACTIVE_BRANCH}", force=True)
+			except GitCommandError as e:
+				print(e)
+				app.log.error(e)
+				return
+
+			await app.send_edit("Successfully Updated, initialing . . .", text_type=["mono"], delme=8)
+
+		else:
+			try:
+				ups_rem.pull(ACTIVE_BRANCH)
+			except GitCommandError:
+				repo.git.reset("--hard", "FETCH_HEAD")
+
+			await install_requirements()
+			await app.send_edit("Successfully updated Userbot!\nBot is restarting . . .", text_type=["mono"], delme=8)
+	except Exception as e:
+		await app.error(e)
 
