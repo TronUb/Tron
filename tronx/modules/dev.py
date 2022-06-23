98acfd0664879537153397fcc703aee59a3b94af
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2022-06-23 21:51:19 +0530

    Update dev.py
    minor

--- tronx/modules/dev.py
+++ tronx/modules/dev.py
@@ -36,45 +36,48 @@ p = print
 async def evaluate_handler(_, m: Message):
 	""" This function is made to execute python codes """
 
-	if app.textlen() > 4096:
-		return await send_edit("Your message is too long ! only 4096 characters are allowed", text_type=["mono"], delme=4)
-
-	global reply, chat_id, chat_type
-
-	reply = m.reply_to_message
-	chat_type = m.chat.type
-	chat_id = m.chat.id
-	text = m.text
-	cmd = text.split(None, 1)[1]
-
-	if not cmd:
-		return await app.send_edit("Give me some text (code) to execute . . .", text_type=["mono"], delme=4)
-
-	msg = await app.send_edit("Running . . .", text_type=["mono"])
-
-	old_stderr = sys.stderr
-	old_stdout = sys.stdout
-	redirected_output = sys.stdout = StringIO()
-	redirected_error = sys.stderr = StringIO()
-	stdout, stderr, exc = None, None, None
-
 	try:
-		await app.aexec(cmd)
-	except Exception:
-		exc = traceback.format_exc()
+		if app.textlen() > 4096:
+			return await send_edit("Your message is too long ! only 4096 characters are allowed", text_type=["mono"], delme=4)
 
-	stdout = redirected_output.getvalue()
-	stderr = redirected_error.getvalue()
-	sys.stdout = old_stdout
-	sys.stderr = old_stderr
-	evaluation = exc or stderr or stdout or "Success"
-	final_output = f"**• COMMAND:**\n\n`{cmd}`\n\n**• OUTPUT:**\n\n`{evaluation.strip()}`"
+		global reply, chat_id, chat_type
 
-	if len(final_output) > 4096:
-		await app.create_file(filename="eval_output.txt", content=str(final_output), caption=f"`{m.text}`")
-		await msg.delete()
-	else:
-		await app.send_edit(final_output)
+		reply = m.reply_to_message
+		chat_type = m.chat.type
+		chat_id = m.chat.id
+		text = m.text
+		cmd = text.split(None, 1)[1]
+
+		if not cmd:
+			return await app.send_edit("Give me some text (code) to execute . . .", text_type=["mono"], delme=4)
+
+		msg = await app.send_edit("Running . . .", text_type=["mono"])
+
+		old_stderr = sys.stderr
+		old_stdout = sys.stdout
+		redirected_output = sys.stdout = StringIO()
+		redirected_error = sys.stderr = StringIO()
+		stdout, stderr, exc = None, None, None
+
+		try:
+			await app.aexec(cmd)
+		except Exception:
+			exc = traceback.format_exc()
+
+		stdout = redirected_output.getvalue()
+		stderr = redirected_error.getvalue()
+		sys.stdout = old_stdout
+		sys.stderr = old_stderr
+		evaluation = exc or stderr or stdout or "Success"
+		final_output = f"**• COMMAND:**\n\n`{cmd}`\n\n**• OUTPUT:**\n\n`{evaluation.strip()}`"
+
+		if len(final_output) > 4096:
+			await app.create_file(filename="eval_output.txt", content=str(final_output), caption=f"`{m.text}`")
+			await msg.delete()
+		else:
+			await app.send_edit(final_output)
+	except Exception as e:
+		await app.error(e)
 
 
 
@@ -82,56 +85,59 @@ async def evaluate_handler(_, m: Message):
 @app.on_message(gen(["term", "shell"], allow =["sudo"]))
 async def terminal_handler(_, m: Message):
 	""" This function is made to run shell commands """
-	if app.long() == 1:
-		return await app.send_edit("Use: `.term pip3 install colorama`", delme=5)
 
-	if app.textlen() > 4096:
-		return await send_edit("Your message is too long ! only 4096 characters are allowed", text_type=["mono"], delme=4)
+	try:
+		if app.long() == 1:
+			return await app.send_edit("Use: `.term pip3 install colorama`", delme=5)
 
-	msg = await app.send_edit("Running . . .", text_type=["mono"])
-	text = m.text.split(None, 1)
-	cmd = text[1]
+		if app.textlen() > 4096:
+			return await send_edit("Your message is too long ! only 4096 characters are allowed", text_type=["mono"], delme=4)
 
-	if "\n" in teks:
-		code = cmd.split("\n")
-		output = ""
-		for x in code:
-			shell = re.split(""" (?=(?:[^'"]|'[^']*'|"[^"]*")*$)""", x)
+		msg = await app.send_edit("Running . . .", text_type=["mono"])
+		text = m.text.split(None, 1)
+		cmd = text[1]
+
+		if "\n" in teks:
+			code = cmd.split("\n")
+			output = ""
+			for x in code:
+				shell = re.split(""" (?=(?:[^'"]|'[^']*'|"[^"]*")*$)""", x)
+				try:
+					process = subprocess.Popen(
+						shell, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+					)
+				except Exception as e:
+					await app.error(e)
+
+				output += "**{}**\n".format(code)
+				output += process.stdout.read()[:-1].decode("utf-8")
+				output += "\n"
+		else:
+			shell = re.split(""" (?=(?:[^'"]|'[^']*'|"[^"]*")*$)""", cmd)
+			for y in range(len(shell)):
+				shell[y] = shell[y].replace('"', "")
 			try:
 				process = subprocess.Popen(
 					shell, stdout=subprocess.PIPE, stderr=subprocess.PIPE
 				)
-			except Exception as e:
-				await app.error(e)
+			except Exception as err:
+				exc_type, exc_obj, exc_tb = sys.exc_info()
+				errors = traceback.format_exception(
+					etype=exc_type, value=exc_obj, tb=exc_tb
+				)
+				return await app.send_edit("**Error:**\n`{}`".format("".join(errors)))
 
-			output += "**{}**\n".format(code)
-			output += process.stdout.read()[:-1].decode("utf-8")
-			output += "\n"
-	else:
-		shell = re.split(""" (?=(?:[^'"]|'[^']*'|"[^"]*")*$)""", cmd)
-		for y in range(len(shell)):
-			shell[y] = shell[y].replace('"', "")
-		try:
-			process = subprocess.Popen(
-				shell, stdout=subprocess.PIPE, stderr=subprocess.PIPE
-			)
-		except Exception as err:
-			exc_type, exc_obj, exc_tb = sys.exc_info()
-			errors = traceback.format_exception(
-				etype=exc_type, value=exc_obj, tb=exc_tb
-			)
-			return await app.send_edit("**Error:**\n`{}`".format("".join(errors)))
+			output = process.stdout.read()[:-1].decode("utf-8")
+		if str(output) == "\n":
+			output = None
 
-		output = process.stdout.read()[:-1].decode("utf-8")
-	if str(output) == "\n":
-		output = None
-
-	if output:
-		if len(output) > 4096:
-			await app.create_file(filename="term_output.txt", content=output, caption=f"`{m.text}`")
+		if output:
+			if len(output) > 4096:
+				await app.create_file(filename="term_output.txt", content=output, caption=f"`{m.text}`")
+			else:
+				await app.send_edit(f"**COMMAND:**\n\n{m.text}\n\n\n**OUTPUT:**\n\n`{output}`")
 		else:
-			await app.send_edit(f"**COMMAND:**\n\n{m.text}\n\n\n**OUTPUT:**\n\n`{output}`")
-	else:
-		await app.send_edit("**OUTPUT:**\n\n`No Output`")
-
+			await app.send_edit("**OUTPUT:**\n\n`No Output`")
+	except Exception as e:
+		await app.error(e)
 
