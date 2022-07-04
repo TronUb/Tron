051fd3a96488e5b98b319a3671fc06d543c5d14f
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2022-07-04 21:51:09 +0530

    Update homepage.py

--- tronx/plugins/callbacks/homepage.py
+++ tronx/plugins/callbacks/homepage.py
@@ -3,13 +3,21 @@ import heroku3
 from pyrogram import filters
 
 from pyrogram.types import (
-	InlineKeyboardButton, 
 	InlineKeyboardMarkup, 
+	InputMediaPhoto,
 	CallbackQuery, 
 	Message,
 )
 
 from tronx import app
+
+
+
+homepage_text = f"""
+**Dex:** Home
+
+**Description:** This is your helpdex use to navigate in different sub dex to information.
+"""
 
 
 
@@ -19,14 +27,13 @@ from tronx import app
 @app.bot.on_callback_query(filters.regex("home-tab"))
 @app.alert_user
 async def _start(_, cb):
-	await cb.edit_message_text(
-		text = "**Dex:** Home\n\n**Description:** This is your helpdex use to navigate in different sub dex to information.",
-		reply_markup=InlineKeyboardMarkup(
-			[
+	await cb.edit_message_media(
+		media=InputMediaPhoto(media=app.BotPic(), caption=homepage_text),
+		reply_markup=InlineKeyboardMarkup([
                 app.BuildKeyboard((["• Settings •", "settings-tab"], ["• Modules •", "modules-tab"])),
                 app.BuildKeyboard((["• Extra •", "extra-tab"], ["• Stats •", "stats-tab"])),
                 app.BuildKeyboard(([["About", "about-tab"]])),
                 app.BuildKeyboard(([["Close", "close-tab"]]))
-            ]
+		]
 		),
 	)
