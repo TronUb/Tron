3ac39f9c72490ee0e4e6697e12c9dc8f525520a0
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2022-07-05 01:56:38 +0530

    Update __main__.py

--- tronx/__main__.py
+++ tronx/__main__.py
@@ -11,29 +11,31 @@ import warnings
 async def start_assistant():
 	""" this function starts the pyrogram bot client. """
 	if app and app.bot:
-		botcmd = [
-			["start", "check whether bot is on or not."],
-			["help", "Get your helpdex."],
-			["ping", "Get server response speed & uptime."],
-			["id", "Get ids of users / groups."],
-			["quote", "get inline anime quotes."],
-			["broadcast", "send messages to users who have started your bot."],
-			["eval", "evaluate python codes."]
-		]
-		cmds = [x.command for x in await app.bot.get_bot_commands()]
-		botcmdkeys = [y[0] for y in botcmd]
-		
-		if cmds != botcmdkeys:
-			print("Setting bot commands.\n")
-			await app.bot.set_bot_commands([[BotCommand(y[0], y[1])] for y in botcmds])
-			print("Added bot commands.\n")
-
 		print("Activating assistant.\n")
 		response = await app.bot.start()
 		if response:
 			print("Assistant activated.\n")
+			botcmd = [
+				["start", "check whether bot is on or not."],
+				["help", "Get your helpdex."],
+				["ping", "Get server response speed & uptime."],
+				["id", "Get ids of users / groups."],
+				["quote", "get inline anime quotes."],
+				["broadcast", "send messages to users who have started your bot."],
+				["eval", "evaluate python codes."]
+			]
+			cmds = [x.command for x in await app.bot.get_bot_commands()]
+			botcmdkeys = [y[0] for y in botcmd]
+		
+			if cmds != botcmdkeys:
+				print("Setting bot commands.\n")
+				await app.bot.set_bot_commands([[BotCommand(y[0], y[1])] for y in botcmds])
+				print("Added bot commands.\n")
+		
 		else:
 			print("Assistant is not activated.\n")
+
+		
 	else:
 		print("Assistant start unsuccessful, please check that you have given the bot token.\n")
 		print("skipping assistant start !")
