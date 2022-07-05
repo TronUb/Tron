1ae72fd37fe6dcac3b027f363870fa7a2580a074
beastzx18 <vs423502@gmail.com>
2022-07-05 19:02:43 +0530

    update

--- tronx/__init__.py
+++ tronx/__init__.py
@@ -3,7 +3,7 @@ import sys
 import platform
 
 
-# detect operating system
+# for termux deployment // check if its android device
 if hasattr(sys, "getandroidapilevel"):
 	ostype = "android"
 	os.system("cd ostype && python3 -m termux")
