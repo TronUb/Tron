8af522c4401e201290351ac899c064947b3b3141
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2022-07-10 20:26:15 +0530

    Update utube.py

--- main/userbot/modules/plugins/utube.py
+++ main/userbot/modules/plugins/utube.py
@@ -43,7 +43,7 @@ async def ytvideoinfo_handler(_, m: Message):
 		thumb_link = yt.thumbnail_url
 		data = f"**Title:** {yt.title}\n\n"
 		data += f"**Duration:** {app.GetReadableTime(yt.length)}\n\n"
-		data += f"**Description:** {yt.description[:4092]}...\n\n"
+		data += f"**Description:** {yt.description[:2000]}...\n\n"
 		data += f"**Views:** {yt.views}\n\n"
 		data += f"**Age Restricted:** {'Yes' if yt.age_restricted else 'No'}"
 
@@ -56,40 +56,34 @@ async def ytvideoinfo_handler(_, m: Message):
 
 @app.on_message(gen("yvdl", allow = ["sudo", "channel"]))
 async def ytvideodl_handler(_, m):
-	reply = m.reply_to_message
-	cmd = m.command
-	await app.send_edit("processing link . . .", text_type=["mono"])
-	if not reply:
-		if app.long() == 1:
-			return await app.send_edit("Please reply to a yt link or give me link as a suffix . . .", text_type=["mono"], delme=4)
-		elif app.long() > 1 and cmd[1]:
-			link = cmd[1]
-		else:
-			return await app.send_edit("Please reply to a link or give me the link as a suffix after command . . .", text_type=["mono"], delme=4)
-	elif reply:
-		if reply.text and reply.entities:
-			entity = reply.entities[0]
-			if isinstance(entity, MessageEntityType.URL):
-				link = reply.text[entity.offset:entity.length+entity.offsef]
+	try:
+		reply = m.reply_to_message
+		cmd = m.command
+		msg = await app.send_edit("processing link . . .", text_type=["mono"])
+		args = app.GetArgs()
+		if args:
+			if args.text and args.text.entities:
+				entity = args.text.entities
+				if entity[0].type == MessageEntityType.URL:
+					i = entity[0]
+					link = args.text[i.offset:i.length+i.offset] # get link from text
+				else:
+					link = args.text
 			else:
-				link = reply.text
-
+				link = args.text
 		else:
-			return await app.send_edit("Please reply to a link or give me the link as a suffix after command . . .", text_type=["mono"], delme=4)
-	else:
-		return await app.send_edit("Something went wrong . . .")
+			return await app.send_edit("Reply or give args after command.", text_type=["mono"], delme=3)
 
-	yt = YouTube(link)
-	data = yt.streams.all()
+		yt = YouTube(link)
+		data = yt.streams.all()
 
-	await app.send_edit("**Trying to download **" + f"`{yt.title}`")
-	for x in data:
-		if x.type == "video" and x.resolution in ("720p" or "1080p") and x.mime_type == "video/mp4":
-			try:
+		await app.send_edit("**Trying to download **" + f"`{yt.title}`")
+		for x in data:
+			if x.type == "video" and x.resolution in ("720p" or "1080p") and x.mime_type == "video/mp4":
 				loc = x.download()
 				await app.send_video(m.chat.id, loc, caption="**Title:**\n\n" + yt.title)
-				await m.delete()
+				await msg.delete()
 				break
-			except Exception as e:
-				await app.error(e)
+	except Exception as e:
+		await app.error(e)
 
