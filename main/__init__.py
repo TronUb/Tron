19fd78b6c94ad22abe5bd2e18f3cc6046dc72256
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2022-07-11 10:42:07 +0530

    Update __init__.py
    pylint changes

--- main/__init__.py
+++ main/__init__.py
@@ -1,14 +1,16 @@
+""" this file determines deployment method """
+
 import os
 import sys
 
 
-# for termux deployments 
+# for termux deployments
 # check if the current device is android
 if hasattr(sys, "getandroidapilevel"):
-	os.system("python3 -m termux")
-	from config import Config
+    os.system("python3 -m termux")
+    from config import Config
 
 
-from main.userbot.client import app
+from main.userbot.client import app # pylint: disable=wrong-import-position
 bot = app.bot
-from main.core.filters import gen, regex
+from main.core.filters import gen, regex # pylint: disable=wrong-import-position
