eee32db567a58286ce3e7a6bf5959850e9d25def
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2022-07-11 21:14:58 +0530

    Update utube.py

--- main/userbot/modules/plugins/utube.py
+++ main/userbot/modules/plugins/utube.py
@@ -132,7 +132,7 @@ async def ytvideodl_handler(_, m):
 						if client.handler:
 							client.remove_handler(*client.handler)
 
-				app.handler = app.add_handler(CallbackQueryHandler(callback=utube_callback, filters=filters.regex(r"\d+")))
+				app.handler = app.add_handler(CallbackQueryHandler(callback=utube_callback, filters=filters.regex(r"\d\d\d")))
 				return True
 
 		video_found = False
