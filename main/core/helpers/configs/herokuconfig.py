e8fcf848fa9e1702448d99ae149b300d299ef9a5
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2022-07-13 15:52:58 +0530

    Create herokuconfig.py

---
+++ main/core/helpers/configs/herokuconfig.py
@@ -0,0 +1,17 @@
+HEROKUDV = [
+    "HEROKU_API_KEY",
+    "HEROKU_APP_NAME"
+    ]
+
+
+
+
+class HerokuConfig(object):
+	def HerokuApiKey(self):
+		""" Set your heroku auth key """
+		return self.getdv("HEROKU_API_KEY") or self.HEROKU_API_KEY or None
+
+
+	def HerokuAppName(self):
+		""" Set your heroku app name """
+		return self.getdv("HEROKU_APP_NAME") or self.HEROKU_APP_NAME or None
