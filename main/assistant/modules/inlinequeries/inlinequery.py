d49cc6cff0eae05c789f8abdeba442f2f5c0917a
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2022-07-13 13:35:43 +0530

    Update inlinequery.py

--- main/assistant/modules/inlinequeries/inlinequery.py
+++ main/assistant/modules/inlinequeries/inlinequery.py
@@ -29,10 +29,8 @@ def inline_result(_, inline_query):
         results=[
             InlineQueryResultPhoto(
                 photo_url=app.PmpermitPic(),
-                title="Tron security system",
-                description="""This is tron security system,
-                    it helps you to stop spammers from spamming in your dm.
-                    """,
+                title="Tron Inline security system",
+                description="Get tron security system inline menu.",
                 caption=app.PmpermitText(),
                 parse_mode=ParseMode.DEFAULT,
                 reply_markup=InlineKeyboardMarkup(
@@ -49,13 +47,9 @@ def inline_result(_, inline_query):
         results=[
             InlineQueryResultPhoto(
                 photo_url=app.BotPic(),
-                title="Introduction to tron",
-                description="This is the tron helpdex menu.",
-                caption="""**Dex:** Home\n\n
-                    **Description:** This is your helpdex use this to navigate in different sub dex. 
-                    guidence and information is given in each dex.
-                    """,
-                parse_mode=ParseMode.DEFAULT,
+                title="Tron Inline helpdex menu",
+                description="Get your inline helpdex menu.",
+                caption=app.home_tab_string(),
                 reply_markup=InlineKeyboardMarkup(
                     [
                         app.BuildKeyboard(
@@ -83,19 +77,9 @@ def inline_result(_, inline_query):
         results=[
             InlineQueryResultPhoto(
                 photo_url=app.ialive_pic(),
-                title="Inline alive",
-                description="""This is same as alive command.
-                    the difference is that this command have inline button.
-                    """,
-                caption=f"""**⛊  Inline Status:**\n\n
-                    **⟐** {app.USER_BIO}\n\n
-                    **⟜ Owner**: [{app.name}](https://t.me/{app.username})\n
-                    **⟜ Tron:** `{app.userbot_version}`\n
-                    **⟜ Python:** `{app.python_version}`\n
-                    **⟜ Pyrogram:** `{app.pyrogram_version}`\n
-                    **⟜ uptime:** `{app.uptime()}\n\n
-                    """,
-                parse_mode=ParseMode.DEFAULT,
+                title="Tron Inline alive",
+                description="Get your inline alive results with buttons.",
+                caption=app.ialive_tab_string(),
                 reply_markup=InlineKeyboardMarkup(
                     [
                         app.BuildKeyboard((["Home", "close-tab"], ["Back", "home-tab"]))
@@ -109,7 +93,7 @@ def inline_result(_, inline_query):
         inline_query.answer(
         results=[
             InlineQueryResultArticle(
-                title="Inline quotes",
+                title="Tron Inline anime quotes",
                 input_message_content=InputTextMessageContent(app.quote()),
                 description="Get infinite anime character quotes through this inline loop button.",
                 reply_markup=InlineKeyboardMarkup(
