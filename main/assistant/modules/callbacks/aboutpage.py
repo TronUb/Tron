e7d0dba19ba984d9d958b5c0912a6817adfc8b52
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2022-07-13 12:50:22 +0530

    Update aboutpage.py

--- main/assistant/modules/callbacks/aboutpage.py
+++ main/assistant/modules/callbacks/aboutpage.py
@@ -15,42 +15,12 @@ from main.userbot.client import app
 
 
 
-ABOUT_TEXT = f"""
-**Dex:** About
-
-**Location:** /home/about
-
-**[ Personal Info ]:**
-
-**Name:** {app.assistant_name}
-
-**[ Versions ]:**
-
-**Python:** {app.python_version}
-**Pyrogram:** {app.pyrogram_version}
-**Assistant:**  {app.assistant_version}
-
-**[ About ]:**
-
-I am Nora made by ࿇•ẞᗴᗩSԵ•࿇.
-I am your friendly assistant, 
-i will help you as much as you need. 
-You can ask me for any help related to your userbot. 
-If you have any suggestions or you're facing any problems 
-which are related to tronuserbot then just ask in 
-[support group](https://t.me/tronuserbot_support)
-"""
-
-
-
-
-
 
 @app.bot.on_callback_query(filters.regex("about-tab"))
 @app.alert_user
 async def _about(_, cb: CallbackQuery):
     await cb.edit_message_media(
-        media=InputMediaPhoto(media="main/core/resources/images/nora.png", caption=ABOUT_TEXT),
+        media=InputMediaPhoto(media="main/core/resources/images/nora.png", caption=app.about_tab_string()),
         reply_markup=InlineKeyboardMarkup(
             [
                 app.bot.BuildKeyboard(
