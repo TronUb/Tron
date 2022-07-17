e3399e90d5ff92e189fe9ccd1bea84bb5d8e9d0c
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2022-07-17 20:12:22 +0530

    Update quotly.py

--- main/userbot/modules/plugins/quotly.py
+++ main/userbot/modules/plugins/quotly.py
@@ -21,32 +21,35 @@ app.CMD_HELP.update(
 
 @app.on_message(gen(["q"], exclude = ["sudo", "channel"]))
 async def quotly_handler(_, m: Message):
-    reply = m.reply_to_message
-    if not reply:
-        return await app.send_edit(m, "Reply to any users text message", delme=4)
-
-    if app.long() > 1:
-        color = m.text.split(None, 1)[1]
-    else:
-        color = "black"
-
-    msg_one = await app.send_edit("Making a Quote . . .", text_type=["mono"])
-    await app.send_message("QuotLyBot", f"/qcolor {color}")
-    await reply.forward("QuotLyBot")
-    is_sticker = True
-    while is_sticker:
-        try:
-            msg = await app.get_last_msg(chat_id="QuotLyBot")
-            if msg.sticker and msg.sticker.file_id:
-                is_sticker = False
-        except Exception:
-            await asyncio.sleep(0.10)
-    if msg.id:
-        await asyncio.gather(
-            msg_one.delete(),
-            app.copy_message(
-                m.chat.id, 
-                "@QuotLyBot", 
-                msg.id
+    try:
+        reply = m.reply_to_message
+        if not reply:
+            return await app.send_edit(m, "Reply to any users text message", delme=4)
+    
+        if app.long() > 1:
+            color = m.text.split(None, 1)[1]
+        else:
+            color = "black"
+    
+        msg_one = await app.send_edit("Making a Quote . . .", text_type=["mono"])
+        await app.send_message("QuotLyBot", f"/qcolor {color}")
+        await reply.forward("QuotLyBot")
+        is_sticker = True
+        while is_sticker:
+            try:
+                msg = await app.get_last_msg(chat_id="QuotLyBot")
+                if msg.sticker and msg.sticker.file_id:
+                    is_sticker = False
+            except Exception:
+                await asyncio.sleep(0.10)
+        if msg.id:
+            await asyncio.gather(
+                msg_one.delete(),
+                app.copy_message(
+                    m.chat.id, 
+                    "QuotLyBot", 
+                    msg.id
                 )
-        )
+            )
+    except Exception as e:
+        await app.error(e)
