0eac9f106d313b6cfa2e8148eac3434ee73848e4
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2022-07-18 23:37:15 +0530

    Update inlinequery.py

--- main/assistant/modules/inlinequeries/inlinequery.py
+++ main/assistant/modules/inlinequeries/inlinequery.py
@@ -162,5 +162,7 @@ def inline_result(_, inline_query):
 
                         else:
                             await cb.answer("You're not allowed to view this message", show_alert=True)
+                    except Exception as e:
+                        await client.error(e)
 
         return app.bot.add_handler(CallbackQueryHandler(callback=whisper_callback, filters=filters.regex(r"\d+[|]\d+")))
