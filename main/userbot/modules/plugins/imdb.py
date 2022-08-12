e3940ec7b7c818a23549a99d4659e758634c35a9
beastzx18 <vs423502@gmail.com>
2022-08-12 18:54:58 +0530

    update

--- main/userbot/modules/plugins/imdb.py
+++ main/userbot/modules/plugins/imdb.py
@@ -42,33 +42,45 @@ async def imdb_handler(_, m: Message):
         box_office = movie["box office"]
         cover_url = app.PyDownload(movie["full-size cover url"])
 
-        caption = f"**Title:** `{movie['long imdb title']}`\n"
-        caption += f"**Duration:** `{movie['runtimes'][0]} min | {movie['year']}`\n"
-        caption += f"**Genres:** {' '.join(f'''`{x}`''' for x in movie['genres'])}\n"
-        caption += f"**Rating:** `{movie['rating']} (by {movie['votes']})`\n"
-        caption += f"**Country:** {' '.join(f'`{x}`' for x in movie['countries'])}\n"
-        caption += f"**Language:** {' '.join(f'`{x}`' for x in movie['languages'])}\n"
-        caption += f"**Director:** {' '.join(f'''`{x['name']}`''' for x in movie['director'])}\n"
-        caption += f"**Music Director:** {' '.join(f'''`{x['name']}`''' for x in movie['composer'])}\n"
-        caption += f"**Writer:** {' '.join(f'''`{x['name']}`''' for x in movie['writer'])}\n"
-        caption += f"**Stars:** {' '.join(f'''`{x['name']}`''' for x in movie['cast'])}\n"
+        # default values
+        nodata = "No Data"
+
+        title = movie.get("long imdb title")
+        year = movie.get("year") if movie.get("year") else nodata
+        duration = movie.get('runtimes')[0] + "min" + f" | {year}" if movie.get('runtimes') else nodata
+        genres = " ".join(f"`{x}`" for x in movie.get("genres")) if movie.get("genres") else nodata
+        votes = movie.get("votes") if movie.get("votes") else nodata
+        rating = movie.get("rating") + " " + f"(by {votes})"
+        country = " ".join(f"`{x}`" for x in movie.get("countries")) if movie.get("countries") else nodata
+        language = " ".join(f"`{x}`" for x in movie.get("languages")) if movie.get("languages") else nodata
+        director = " ".join(f"`{x['name']}`" for x in movie.get("director")) if movie.get("director") else nodata
+        music_director = " ".join(f"`{x['name']}`" for x in movie.get("composer")) if movie.get("composer") else nodata
+        writer = " ".join(f"`{x['name']}`" for x in movie.get('writer')) if movie.get('writer') else nodata
+        stars = " ".join(f"`{x['name']}`" for x in movie.get("cast")) if movie.get("cast") else nodata
+        budget = box_office.get("Budget") if box_office.get("Budget") else nodata
+        owus = box_office["Opening Weekend United States"] if box_office["Opening Weekend United States"] else nodata
+        cwg = box_office["Cumulative Worldwide Gross"] if box_office["Cumulative Worldwide Gross"] else nodata
+        plot = "".join(movie.get("plot")) if movie.get("plot") else nodata
+
+        caption = f"**Title:** `{title}`\n"
+        caption += f"**Duration:** `{duration}`\n"
+        caption += f"**Genres:** {genres}\n"
+        caption += f"**Rating:** `{rating}`\n"
+        caption += f"**Country:** {country}\n"
+        caption += f"**Language:** {language}\n"
+        caption += f"**Director:** {director}\n"
+        caption += f"**Music Director:** {music_director}\n"
+        caption += f"**Writer:** {writer}\n"
+        caption += f"**Stars:** {stars}\n"
         caption += "**Box Office:** \n"
-        caption += f"> **Budget:** `{box_office['Budget']}`\n"
-        caption += f"> **Opening Weekend United States:** `{box_office['Opening Weekend United States']}`\n"
-        caption += f"> **Cumulative Worldwide Gross:** `{box_office['Cumulative Worldwide Gross']}`\n"
-        caption += f"**Story Outline:** `{movie['plot'][0][:100]+'...'}`\n"
-        
-        link = app.telegraph.create_page(
-            app.name,
-            html_content=caption
-        )
-        
-        url = f"https://telegra.ph/{link.get('path')}"
+        caption += f"> **Budget:** `{budget}`\n"
+        caption += f"> **Opening Weekend United States:** `{owus}`\n"
+        caption += f"> **Cumulative Worldwide Gross:** `{cwg}`\n"
 
         complete = await app.send_photo(
             m.chat.id,
             photo=cover_url,
-            caption=f"[Results]({url})"
+            caption=caption
         )
 
         if complete:
