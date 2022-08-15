95f1d39cbd57b3ce12ceafb0d5364badb835f05d
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2022-08-15 23:07:17 +0530

    Update videotools.py

--- main/userbot/modules/plugins/videotools.py
+++ main/userbot/modules/plugins/videotools.py
@@ -37,21 +37,21 @@ async def videocut_handler(_, m: Message):
             "Processing . . .",
             text_type=["mono"]
         )
-    
+
         if not reply:
             return await app.send_edit(
                 "Reply to a video . . .",
                 text_type=["mono"],
                 delme=3
             )
-    
+
         if not reply.video:
             return await app.send_edit(
                 "Reply to a video . . .",
                 text_type=["mono"],
                 delme=3
             )
-    
+
         args = app.GetArgs(m)
         if not args:
             return await app.send_edit(
@@ -59,9 +59,9 @@ async def videocut_handler(_, m: Message):
                 text_type=["mono"],
                 delme=3
             )
-    
-        cut_time = args.text.split(None, 1)[0]
-    
+
+        cut_time = args.text.split(None, 1)[1]
+
         await app.send_edit(
             "Downloading video . . .",
             text_type=["mono"]
@@ -71,15 +71,15 @@ async def videocut_handler(_, m: Message):
             "Cutting video . . .",
             text_type=["mono"]
         )
-    
+
         clip = clip.set_duration(cut_time)
         await app.send_edit(
             "Saving new video . . .",
             text_type=["mono"]
         )
-    
+
         clip.write_videofile(vname)
-    
+
         if os.path.exists(vname):
             msg = await app.send_edit(
                 "Ssnding new video . . .",
