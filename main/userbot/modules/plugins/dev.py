84cc149955234a5d81683b4685d38cc65ddb1a20
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2022-08-20 14:56:36 +0530

    Update dev.py

--- main/userbot/modules/plugins/dev.py
+++ main/userbot/modules/plugins/dev.py
@@ -48,8 +48,11 @@ async def evaluate_handler(_, m: Message):
                 text_type=["mono"],
                 delme=4
             )
+        if m.sudo_message:
+            text = m.sudo_message.text
+        else:
+            text = m.text
 
-        text = m.text
         cmd = text.split(None, 1)[1]
 
         msg = await app.send_edit("Executing . . .", text_type=["mono"])
