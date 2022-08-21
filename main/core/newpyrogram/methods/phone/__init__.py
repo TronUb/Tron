22a9c6b54b4af993e70def5221eecc1c5acae251
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2022-08-22 01:47:20 +0530

    Update __init__.py

--- main/core/newpyrogram/methods/phone/__init__.py
+++ main/core/newpyrogram/methods/phone/__init__.py
@@ -1,10 +1,12 @@
 from .create_group_call import CreateGroupCall
 from .get_group_call import GetGroupCall
+from .join_group_call import JoinGroupCall
 
 
 
 class Phone(
     CreateGroupCall,
-    GetGroupCall
+    GetGroupCall,
+    JoinGroupCall
 ):
     pass
