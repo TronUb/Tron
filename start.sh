3fb0b923235076187952a60bbc359246f325ee4a
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2022-08-28 15:15:19 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,52 +4,77 @@
 
 oname=$(uname -o)
 filename="./config.txt"
+repo="https://github.com/TronUb/Tron.git"
+ffmpeg=$(ffmpeg)
+nodejs=$(node)
+python=$(python3 --version)
+pip=$(pip --version)
+pytgcalls=$(pip3 show py-tgcalls)
+git=$(git)
 
-if [[ "$oname" == "Android" && ! -f "$filename" ]]; then
+
+# not found
+no_file="No such file or directory"
+no_cmd="not found"
+
+
+echo $'Welcome to Tron Corporation\n'
+
+if [ "$oname" == "Android" ]; then
     clear
-    echo $'Welcome To Tron Corporation.\n\n'
-    echo $'Starting Tron Deployment (Termux) . . .\n\n'
-    echo $'Installing Ubuntu 20 in Termux . . .\n'
-    bash install_ubuntu20.sh
-    clear
-    echo $'Starting Ubuntu 20 . . .\n'
-    bash start-ubuntu20.sh
-    clear
-    echo $'Updating & Upgrading Ubuntu . . .\n'
-    apt update && apt upgrade 
-    clear
-    echo $'Installing git . . .\n'
-    apt install git
-    clear
-    echo $'Cloning Tron Repo . . .\n'
-    git clone https://github.com/TronUb/Tron.git
-    clear
-    echo $'Changing directory . . .\n'
-    cd && cd Tron
-    clear
-    echo $'Installing python3 & pip3 . . .\n'
-    apt install python3 python3-pip
-    clear
-    echo $'Installing dependencies from requirements.txt\n'
-    pip3 install -r requirements.txt
-    clear
-    echo $'Installing nodejs . . .\n'
-    bash install_nodejs.sh
-    clear
-    echo $'Installing ffmpeg . . .\n'
-    apt install ffmpeg
-    clear
-    echo $'Installing py-tgcalls . . .\n'
-    python -m pip install pytgcalls
-    clear
+    # update & upgrade ubuntu
+    apt update && apt upgrade
+
+    # install python3
+    if [[ $python =~ no_cmd ]]; then
+       echo $'Installing python3 . . .\n'
+       apt install python3
+       clear
+    fi
+
+    # install python3 pip
+    if [[ $pip =~ no_cmd ]]; then
+       echo $'Installing python3 pip . . .\n'
+       apt install pip
+       clear
+    fi
+
+    # install git 
+    if [[ $git =~ no_file ]]; then
+        echo $'Installing git . . .\n'
+        apt install git
+        clear
+    fi
+
+    # clone Tron repo
+    if [ ! -f "$filename" ]; then
+        git clone $repo
+        clear
+        cd Tron
+    else
+        cd Tron
+    fi
+
+    # install ffmpeg
+    if [[ $ffmpeg =~ $error ]]; then
+        apt install ffmpeg
+        clear
+    fi
+
+    # install nodejs
+    if [[ $nodejs =~ $error ]]; then
+        bash installl_nodejs.sh
+        clear
+    fi
+
+    # install py-tgcalls
+    if [[ $pytgcalls =~ no_cmd ]]; then
+        echo 'Installing py-tgcalls . . .\n'
+        python3 -m pip install py-tgcalls
+        clear
+    fi
+
     python3 -m main
-elif [[ "$oname" == "Android" && -f "$filename" ]]; then
-    python3 -m main
-elif [ "$oname" == "Android" ]; then
-    clear
-    echo $'The config.txt file doesnt exist !\n'
-    echo $'Set your config.txt file with config values.'
-    exit
 else
     python3 -m main
 fi
