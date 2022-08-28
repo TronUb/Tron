cfacd32b90b56b1ce26da556aa6fa1cb5bb1526b
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2022-08-28 13:46:50 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,39 +7,39 @@ filename="./config.txt"
 
 if [[ "$oname" == "Android" && ! -f "$filename" ]]; then
     clear
-    echo "Welcome To Tron Corporation.\n\n"
-    echo "Starting Tron Deployment (Termux) . . .\n\n"
-    echo "Installing Ubuntu 20 in Termux . . .\n"
+    echo $'Welcome To Tron Corporation.\n\n'
+    echo $'Starting Tron Deployment (Termux) . . .\n\n'
+    echo $'Installing Ubuntu 20 in Termux . . .\n'
     bash install_ubuntu20.sh
     clear
-    echo "Starting Ubuntu 20 . . .\n"
+    echo $'Starting Ubuntu 20 . . .\n'
     bash start-ubuntu20.sh
     clear
-    echo "Updating & Upgrading Ubuntu . . .\n"
+    echo $'Updating & Upgrading Ubuntu . . .\n'
     apt update && apt upgrade 
     clear
-    echo "Installing git . . .\n"
+    echo $'Installing git . . .\n'
     apt install git
     clear
-    echo "Cloning Tron Repo . . .\n"
+    echo $'Cloning Tron Repo . . .\n'
     git clone https://github.com/TronUb/Tron.git
     clear
-    echo "Changing directory . . .\n"
+    echo $'Changing directory . . .\n'
     cd && cd Tron
     clear
-    echo "Installing python3 & pip3 . . .\n"
+    echo $'Installing python3 & pip3 . . .\n'
     apt install python3 python3-pip
     clear
-    echo "Installing dependencies from requirements.txt\n"
+    echo $'Installing dependencies from requirements.txt\n'
     pip3 install -r requirements.txt
     clear
-    echo "Installing nodejs . . .\n"
+    echo $'Installing nodejs . . .\n'
     bash install_nodejs.sh
     clear
-    echo "Installing ffmpeg . . .\n"
+    echo $'Installing ffmpeg . . .\n'
     apt install ffmpeg
     clear
-    echo "Installing py-tgcalls . . .\n"
+    echo $'Installing py-tgcalls . . .\n'
     python -m pip install pytgcalls
     clear
     python3 -m main
@@ -47,8 +47,8 @@ elif [[ "$oname" == "Android" && -f "$filename" ]]; then
     python3 -m main
 elif [ "$oname" == "Android" ]; then
     clear
-    echo "The config.txt file doesn't exist !"
-    echo "Set your config.txt file with config values."
+    echo $'The config.txt file doesn't exist !\n'
+    echo $'Set your config.txt file with config values.'
     exit
 else
     python3 -m main
