8c9a3b97fb77bd828e5210994b4886693d6d56a2
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2022-08-30 23:24:26 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -1,13 +1,5 @@
 """ everything starts here """
 
-import os
-
-try:
-    import pytgcalls
-except ImportError:
-    os.system("pip3 install pytgcalls")
-
-
 from config import Config
 from main.userbot.client import app
 bot = app.bot
