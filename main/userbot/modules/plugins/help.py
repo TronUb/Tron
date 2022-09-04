cee6199e8268e88b04ac9d51e714a1b7805b88d7
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2022-09-05 01:34:34 +0530

    Update help.py

--- main/userbot/modules/plugins/help.py
+++ main/userbot/modules/plugins/help.py
@@ -54,7 +54,7 @@ async def delete_helpdex(_, cb: CallbackQuery):
                 chat_id=int(str(-100) + str(chat_id)[1:]),
                 message_ids=message_id
             )
-    except PeerIdInvalid:
+    except (PeerIdInvalid, KeyError):
         await app.delete_messages(
             chat_id=chat_id,
             message_ids=message_id
