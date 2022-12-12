b14d49ff735fdb5103d84cd8568446212190e84b
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2022-12-12 23:54:35 +0530

    Update client.py

--- main/assistant/client/client.py
+++ main/assistant/client/client.py
@@ -3,7 +3,9 @@ This file creates Assistant's client.
 """
 
 from pyrogram import Client
+from pyrogram.errors import FloodWait
 from main.core import Core
+
 
 
 
@@ -19,14 +21,18 @@ class Bot(Core, Client):
             bot_token=self.BOT_TOKEN,
             in_memory=True
 	)
-        self.start()
-        self.me = self.get_chat("me")
-        self.id = self.me.id
-        self.dc_id = self.me.dc_id
-        self.name = self.me.first_name
-        self.username = f"@{self.me.username}"
-        self.bio = self.me.bio if self.me.bio else ""
-        self.pic = self.download_media(self.me.photo.big_file_id) if self.me.photo else None
-        self.is_bot = True
-        self.stop()
+        try:
+            self.start()
+            self.me = self.get_chat("me")
+            self.id = self.me.id
+            self.dc_id = self.me.dc_id
+            self.name = self.me.first_name
+            self.username = f"@{self.me.username}"
+            self.bio = self.me.bio if self.me.bio else ""
+            self.pic = self.download_media(self.me.photo.big_file_id) if self.me.photo else None
+            self.is_bot = True
+            self.stop()
+        except FloodWait as e:
+            asyncio.sleep(e.value)
+
         self.__class__.__module__ = "pyrogram.client"
