53697fc1315a4f738960d912d2e1addfd6e0b4b8
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2022-12-20 04:30:32 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,12 +18,88 @@ import os
 import platform
 
 
-class tools:
+class Config:
+    pass
+
+
+class Tools:
     device = platform.uname()[0].lower()
     is_linux = (device=="linux")
     is_windows = (device=="windows")
-    
 
-    
+    def check_command(self, args: list):
+        return (subprocess.run(
+            args,
+            stdout=subprocess.PIPE,
+            shell=True
+        )).stdout.decode()
 
 
+    def requirements(self):
+        with open("requirements.txt", "r") as f:
+            return [x for x in f.read().split("\n") if x not in ("\n", "")]
+
+
+    def check_requirements(self):
+        print("Checking Packages:\n\n")
+        for pkg in requirements():
+            try:
+                pkg_resources.require([pkg])
+            except pkg_resources.DistributionNotFound as e:
+                print(f"Since {e.req} is not Installed, Installing {e.req}")
+                os.system(f"pip3 install {e.req}")
+
+
+tools = Tools()
+
+tools.check_requirements()
+if tools.is_windows:
+    # install ffmpeg
+
+    # permission needed in windows
+    os.system('Set-ExecutionPolicy RemoteSigned -Scope CurrentUser')
+    # install scoop for installing scoop & other packages
+    os.system('Invoke-Expression "& {$(Invoke-RestMethod get.scoop.sh)} -RunAsAdmin"')
+    # install ffmpeg through scoop
+    os.system('scoop install ffmpeg')
+
+elif tools.is_linux:
+    # install ffmpeg
+
+    os.system('apt install ffmpeg')
+
+else:
+    print('\nUnknown device, Existing . . .')
+    exit(0)
+
+
+# variable counter
+count = 1
+
+# check if the user config file exists
+if os.path.exists("config.text"):
+print("config.text file exists: Yes\n\n")
+    with open("config.text") as f:
+        content = [x for x in f.read().split("\n") if x not in ("\n", "")]
+
+    # set text file config values
+    print("Setting configuration values.\n\n")
+    for x in content:
+        data = x.split("=")
+        file_value = data[1]
+        if data[1].isdigit():
+            file_value = int(data[1])
+
+        setattr(Config, data[0], file_value)
+        print(f"[{count}] Added config = {data[0]} with value = {file_value}\n")
+        count += 1
+
+# set remaining necessary config values
+print("Setting remaining configuration values\n\n")
+for attr in dir(Configuration):
+    value = getattr(Configuration, attr, None)
+
+    if attr.isupper() and not hasattr(Config, attr):
+        setattr(Config, attr, value)
+        print(f"[{count}] Added config = {attr} with value = {value}\n")
+        count += 1
