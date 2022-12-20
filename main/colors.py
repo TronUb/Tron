10790287cf3181f9bb5d257da281eb8e6ef44934
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2022-12-20 21:30:27 +0530

    Create colors.py

---
+++ main/colors.py
@@ -0,0 +1,22 @@
+""" All colors """
+
+
+
+class Colors:
+    # colours
+    black = "\u001b[30m"
+    red = "\u001b[31m"
+    green = "\u001b[32m"
+    yellow = "\u001b[33m"
+    blue = "\u001b[34m"
+    magenta = "\u001b[35m"
+    cyan = "\u001b[36m"
+    white = "\u001b[37m"
+
+    # decorators
+    bold = "\u001b[1m"
+    underline = "\u001b[0m\u001b[4m"
+    reversed = "\u001b[0m\u001b[7m"
+
+    # reset
+    reset = "\u001b[0m"
