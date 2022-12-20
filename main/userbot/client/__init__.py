96569946fc73b642c45a92205919a72ae1ec636d
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2022-12-20 23:12:54 +0530

    Update __init__.py

--- main/userbot/client/__init__.py
+++ main/userbot/client/__init__.py
@@ -6,7 +6,7 @@ try:
     import uvloop
     uvloop.install()
 except ImportError:
-    print(Colors.block + Colors.red + "uvloop wasn't installed, userbot will work slow." + Colors.reset)
+    print(Colors.block + Colors.red + "\nuvloop wasn't installed, userbot will work slow.\n" + Colors.reset)
 
 from .client import SuperClient
 
