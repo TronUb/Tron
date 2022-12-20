c0a8801457fdbfa4d1d34d6922275df0b707f7e4
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2022-12-21 01:38:23 +0530

    Update __main__.py

--- main/__main__.py
+++ main/__main__.py
@@ -62,7 +62,7 @@ async def start_userbot():
         response = await app.start()
         if response:
             print(Colors.cursor_left(5)
-            print(f"[{Colors.green}ON{Colors.reset}]", end=\n\n")
+            print(f"[{Colors.green}ON{Colors.reset}]", end="\n\n")
         else:
             print("Userbot is not activated.\n")
     else:
