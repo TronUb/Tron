a79fd3afd093fa52844147fbc05517bb24425d9b
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2022-12-20 14:30:55 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -3,11 +3,15 @@
 import os
 import time
 import socket
+import logging
 import platform
 import subprocess
 import pkg_resources
 from config import Configuration
 
+
+
+log = logging.getLogger(__name__)
 
 
 class Config:
@@ -45,12 +49,12 @@ class Tools:
 
     def check_requirements(self):
         self.install_ffmpeg()
-        print("Checking Packages:\n\n")
+        log.info("Checking Packages:\n\n")
         for pkg in self.requirements():
             try:
                 pkg_resources.require([pkg])
             except pkg_resources.DistributionNotFound as e:
-                print(f"\nSince {e.req} is not Installed, Installing {e.req}")
+                log.info(f"\nSince {e.req} is not Installed, Installing {e.req}")
                 if e.req == "numpy":
                     self.install_numpy()
 
@@ -68,7 +72,7 @@ class Tools:
 
 
     def install_numpy(self):
-        print("\nInstalling numpy . . .\n")
+        log.info("\nInstalling numpy . . .\n")
         os.system('MATHLIB="m" python -m pip install numpy')
 
 
@@ -78,7 +82,7 @@ class Tools:
             os.system("scoop install libxslt")
         else:
             os.system("apt install libxml2 libxslt")
-        print("\nInstalling lxml . . .\n")
+        log.info("\nInstalling lxml . . .\n")
         os.system("python -m pip install lxml")
 
 
@@ -87,7 +91,7 @@ class Tools:
             os.system("scoop install postgresql python make clang")
         else:
             os.system("apt install postgresql python make clang")
-        print("\nInstalling psycopg2 . . .\n")
+        log.info("\nInstalling psycopg2 . . .\n")
         os.system("python -m pip install psycopg2")
 
 
@@ -98,7 +102,7 @@ class Tools:
         else:
             os.system("apt install libjpeg-turbo")
             os.system('LDFLAGS="-L/system/lib/" CFLAGS="-I/data/data/com.termux/files/usr/include/"')
-        print("\nInstalling pillow . . .")
+        log.info("\nInstalling pillow . . .")
         os.system("python -m pip install pillow")
 
     def install_ffmpeg(self):
@@ -118,7 +122,7 @@ class Tools:
             os.system('apt install ffmpeg')
 
         else:
-            print('\nUnknown device, Existing . . .')
+            log.info('\nUnknown device, Existing . . .')
             exit(0)
 
     def setup_config(self):
@@ -127,12 +131,12 @@ class Tools:
 
         # check if the user config file exists
         if os.path.exists("config.text"):
-            print("config.text file exists: Yes\n\n")
+            log.info("config.text file exists: Yes\n\n")
             with open("config.text") as f:
                 content = [x for x in f.read().split("\n") if x not in ("\n", "")]
 
             # set text file config values
-            print("Setting configuration values.\n\n")
+            log.info("Setting configuration values.\n\n")
             for x in content:
                 data = x.split("=")
                 file_value = data[1]
@@ -140,24 +144,24 @@ class Tools:
                     file_value = int(data[1])
 
                 setattr(Config, data[0], file_value)
-                print(f"[{count}] Added config = {data[0]} with value = {file_value}\n")
+                log.info(f"[{count}] Added config = {data[0]} with value = {file_value}\n")
                 count += 1
 
 
             # install requirements before running bot
             self.check_requirements()
         else:
-            print("config.text file doesn't exist, existing. . .")
+            log.info("config.text file doesn't exist, existing. . .")
             exit(0)
 
         # set remaining necessary config values
-        print("\nSetting remaining configuration values\n\n")
+        log.info("\nSetting remaining configuration values\n\n")
         for attr in dir(Configuration):
             value = getattr(Configuration, attr, None)
 
             if attr.isupper() and not hasattr(Config, attr):
                 setattr(Config, attr, value)
-                print(f"[{count}] Added config = {attr} with value = {value}\n")
+                log.info(f"[{count}] Added config = {attr} with value = {value}\n")
                 count += 1
 
 
