646d6dcf3d3ff5d3173f6c0ee55ebdf9ec327b1d
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2022-12-24 12:31:49 +0530

    Update inlinequery.py

--- main/assistant/modules/inlinequeries/inlinequery.py
+++ main/assistant/modules/inlinequeries/inlinequery.py
@@ -67,7 +67,8 @@ async def inline_result(_, inline_query):
         await inline_query.answer(
         results=[
             InlineQueryResultVideo(
-                video_url="main/core/resources/videos/Nora.mp4",
+                video_url="main/core/resources/videos/nora.mp4",
+                thumb_url="main/core/resources/images/nora.jpeg",
                 title="Tron Inline helpdex menu",
                 description="Get your inline helpdex menu.",
                 caption=app.home_tab_string(),
