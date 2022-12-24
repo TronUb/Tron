801c1fa79e158f09f9224692076f60845fc481f3
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2022-12-24 19:13:06 +0530

    Update botconfig.py

--- main/core/helpers/configs/botconfig.py
+++ main/core/helpers/configs/botconfig.py
@@ -45,4 +45,4 @@ class BotConfig(object):
 
     def BotPic(self):
         """Get your bot pic url"""
-        return self.getdv("BOT_PIC") or self.BOT_PIC or "https://raw.githubusercontent.com/TronUb/Tron/beta/main/core/resources/images/homepage.png" or None
+        return self.getdv("BOT_PIC") or self.BOT_PIC or "https://raw.githubusercontent.com/TronUb/Tron/master/main/core/resources/images/homepage.png" or None
