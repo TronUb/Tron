738b3ed505b72e0e22ea035b313dcbf2a2afaf44
vs423502@gmail.com <vs423502@gmail.com>
2023-01-03 01:04:21 +0530

    .

--- main/core/methods/database/__init__.py
+++ main/core/methods/database/__init__.py
@@ -1,8 +1,8 @@
-from main.core.database.postgresql.afk_sql import AFKSQL
-from main.core.database.postgresql.notes_sql import NOTESSQL
-from main.core.database.postgresql.pmpermit_sql import PMPERMITSQL
-from main.core.database.postgresql.dv_sql import DVSQL
-from main.core.database.postgresql.welcome_sql import WELCOMESQL
+from .postgresql.afk_sql import AFKSQL
+from .postgresql.notes_sql import NOTESSQL
+from .postgresql.pmpermit_sql import PMPERMITSQL
+from .postgresql.dv_sql import DVSQL
+from .postgresql.welcome_sql import WELCOMESQL
 
 
 
