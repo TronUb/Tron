1163e251968a4b7cafe7e5e7e324260771274d41
vs423502@gmail.com <vs423502@gmail.com>
2023-01-08 01:00:14 +0530

    .

--- main/core/methods/decorators/alert_user.py
+++ main/core/methods/decorators/alert_user.py
@@ -9,7 +9,7 @@ class AlertUser(object):
     def alert_user(self, func):
         async def wrapper(_, cb: CallbackQuery):
             user = cb.from_user
-            if user and not (user.id == self.id or user.id in self.SudoUsersList()):
+            if user and not (user.id == self.id or user.id in self.SudoUsersList):
                 await cb.answer(
                     f"Sorry, but you can't use this userbot ! make your own userbot at @tronuserbot", 
                     show_alert=True
