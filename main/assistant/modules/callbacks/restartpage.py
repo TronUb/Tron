0edd5b9ad301aa0a44e1fb1cc0f8bd2b6e2bd37a
vs423502@gmail.com <vs423502@gmail.com>
2023-01-09 23:46:21 +0530

    .

--- main/assistant/modules/callbacks/restartpage.py
+++ main/assistant/modules/callbacks/restartpage.py
@@ -17,66 +17,69 @@ from main.userbot.client import app
 
 
 
-@app.bot.on_callback_query(filters.regex("restart-tab"))
+@app.bot.on_callback_query(filters.regex(r"restart-tab(.[a-z]+)?"))
 @app.alert_user
 async def _restart_userbot(_, cb: CallbackQuery):
-    await cb.edit_message_text(
-        text=app.restart_tab_string("`Press confirm to restart.`"),
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        text="Confirm",
-                        callback_data="confirm-restart-tab"
-                    )
-                ],
-                [
-                    InlineKeyboardButton(
-                        text="Home",
-                        callback_data="close-tab"
-                    ),
-                    InlineKeyboardButton(
-                        text="Back",
-                        callback_data="settings-tab"
-                    )
-                ]
-            ]
-        ),
-    )
-
-
-@app.bot.on_callback_query(filters.regex("confirm-restart-tab"))
-@app.alert_user
-async def _confirm_restart(_, cb: CallbackQuery):
-    try:
-        back_button = InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        text="Back",
-                        callback_data="settings-tab"
-                    )
-                ]
-            ]
+    if not app.herokuApp:
+        return await cb.answer(
+            "Sorry you are not on Heroku Platform.",
+            show_alert=True
         )
 
+    if cb.matches[0].match=="restart-tab-confirm":
+        try:
+            back_button = InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(
+                            text="Back",
+                            callback_data="settings-tab"
+                        )
+                    ]
+                ]
+            )
+
+            await cb.edit_message_text(
+                text=app.restart_tab_string("`Trying to restart userbot . . .`"),
+                reply_markup=back_button
+            )
+            if not app.heroku_app():
+                await cb.edit_message_text(
+                    text=app.restart_tab_string("`Heroku requirements missing (heroku - key, app name), restart manually . . .`"),
+                    reply_markup=back_button
+                )
+            else:
+                res = app.herokuApp.restart()
+                text = "`Please wait 2-3 minutes to restart userbot . . .`"
+                final_text = text if res else "`Failed to restart userbot, do it manually . . .`"
+                await cb.edit_message_text(
+                    text=app.restart_tab_string(final_text),
+                    reply_markup=back_button
+                )
+        except Exception as e:
+            print(e)
+            await app.error(e)
+    else:
         await cb.edit_message_text(
-            text=app.restart_tab_string("`Trying to restart userbot . . .`"),
-            reply_markup=back_button
+            text=app.restart_tab_string("`Press confirm to restart.`"),
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(
+                            text="Confirm",
+                            callback_data="confirm-restart-tab"
+                        )
+                    ],
+                    [
+                        InlineKeyboardButton(
+                            text="Home",
+                            callback_data="close-tab"
+                        ),
+                        InlineKeyboardButton(
+                            text="Back",
+                            callback_data="settings-tab"
+                        )
+                    ]
+                ]
+            ),
         )
-        if not app.heroku_app():
-            await cb.edit_message_text(
-                text=app.restart_tab_string("`Heroku requirements missing (heroku - key, app name), restart manually . . .`"),
-                reply_markup=back_button
-            )
-        else:
-            res = app.herokuApp.restart()
-            text = "`Please wait 2-3 minutes to restart userbot . . .`"
-            final_text = text if res else "`Failed to restart userbot, do it manually . . .`"
-            await cb.edit_message_text(
-                text=app.restart_tab_string(final_text),
-                reply_markup=back_button
-            )
-    except Exception as e:
-        print(e)
-        await app.error(e)
