419b9c23c91e3690a005a419fe6a6cbcf5dc7374
vs423502@gmail.com <vs423502@gmail.com>
2023-01-13 15:53:05 +0530

    .

--- main/userbot/modules/plugins/afk.py
+++ main/userbot/modules/plugins/afk.py
@@ -27,70 +27,56 @@ async def afk_handler(_, m: Message):
         start = int(time.time())
         if app.long() >= 2:
             reason = m.text.split(None, 1)[1]
-            app.set_afk(True, reason, start) # with reason
-            add_afkhandler()
-            await app.send_edit(f"{app.UserMention} is now Offline.\nBecause: {reason}", delme=3)
-
-        elif app.long() == 1 and app.long() < 4096:
+        elif app.long() == 1:
             reason = app.AfkText
+        else:
+            reason = ""
 
-            if reason:
-                app.set_afk(True, reason, start) # with reason
-                add_afkhandler()
-                await app.send_edit(
-                    f"{app.UserMention} is now offline.\nBecause: {reason}",
-                    delme=3
-                )
-            else:
-                app.set_afk(True, "", start) # without reason
-                add_afkhandler()
-                await app.send_edit(f"{app.UserMention} is now offline.", delme=3)
+        app.set_afk(True, reason, start)
+        add_afkhandler()
+        if reason:
+            reason = f"Because: {reason}"
 
+        await app.send_edit(f"{app.UserMention} is now Offline.\n{reason}", delme=3)
     except Exception as e:
         await app.error(e)
 
 
 
-# notify mentioned users
+# while you are afk
+# notify the users who mention or tag you
 async def offlinemention_handler(_, m: Message):
     try:
         get = app.get_afk()
-        if get and get["afk"]:
-            if "-" in str(m.chat.id):
-                cid = str(m.chat.id)[4:]
-            else:
-                cid = str(m.chat.id)
 
-            end = int(time.time())
-            otime = app.GetReadableTime(end - get["afktime"])
-            if get["reason"] and get["afktime"]:
-                await app.send_message(
-                    m.chat.id,
-                    f"Sorry {app.UserMention} is currently offline !\n**Time:** {otime}\n**Because:** {get['reason']}",
-                    reply_to_message_id=m.id
-                    )
-                await app.delete_message(3)
-            elif get["afktime"] and not get["reason"]:
-                await app.send_message(
-                    m.chat.id,
-                    f"Sorry {app.UserMention} is currently offline !\n**Time:** {otime}",
-                    reply_to_message_id=m.id
-                    )
-                await app.delete_message(3)
+        if not (get or get["afk"]):
+            return None
 
-            text = m.text if m.text else ""
-            cid = m.chat.id if m.chat and m.chat.id else 0
+        reason = f"**Because:** {reason}" if get["reason"] else ""
 
-            await app.send_message(
-                app.LOG_CHAT,
-                f"""#mention\n\n
-                **User:** `{m.from_user.first_name}`\n
-                **Id:** {m.from_user.id}\n
-                **Group:** `{m.chat.title}`\n
-                **Message:** `{text[:4096]}`\n
-                [Go to message]({m.link})
-                """
-                )
+        await app.send_message(
+            m.chat.id,
+            f"""
+            Sorry {app.UserMention} is currently offline !\n
+            **Time:** {app.GetReadableTime(int(time.time()) - get["afktime"])}\n
+            {reason}
+            """,
+            reply_to_message_id=m.id
+        )
+
+        text = m.text if m.text else "" # could be a media so
+
+        await app.send_message(
+            app.LOG_CHAT,
+            f"""
+            #mention\n\n
+            **User:** `{m.from_user.first_name}`\n
+            **Id:** `{m.from_user.id}`\n
+            **Group:** `{m.chat.title}`\n
+            **Message:** `{text[:4096]}`\n
+            [Go to message]({m.link})
+            """
+        )
     except Exception as e:
         await app.error(e)
 
