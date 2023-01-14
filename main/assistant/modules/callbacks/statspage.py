21456888f38c0d2938e4010ea53831d1462c02a1
vs423502@gmail.com <vs423502@gmail.com>
2023-01-15 02:12:51 +0530

    update

--- main/assistant/modules/callbacks/statspage.py
+++ main/assistant/modules/callbacks/statspage.py
@@ -10,14 +10,13 @@ from main.userbot.client import app
 
 
 
-
 @app.bot.on_callback_query(filters.regex("stats-tab"))
 @app.alert_user
 async def stats_callback(_, cb: CallbackQuery):
     await cb.edit_message_text(
         text=app.stats_tab_string,
         reply_markup=app.buildMarkup(
-            [app.buildButton("Home", "close-tab")],
+            [app.buildButton("refresh", "stats-tab")],
             [app.buildButton("Back", "home-tab")]
         )
     )
