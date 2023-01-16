cf691cf5b700d93e3acf9d6ecf4af5aa0b276be8
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2023-01-16 17:41:40 +0530

    Update session.py

--- session.py
+++ session.py
@@ -19,26 +19,35 @@ Requirements:
   2. API_HASH
   3. PHONE NUMBER (WITH COUNTRY CODE, EX: +911034567891)
   
+  NOTE: Enter "Ctrl + C" to exit
+\n
 """
 
 
 print(intro)
 
 
-API_ID = input("\nEnter your API_ID: ")
+while True:
+  API_ID = input("Enter your API_ID: ")
 
-while not (API_ID.isdigit() and len(API_ID) == 7):
-  print("\n\nPlease enter a valid 7 digit API_ID.\n\n")
-  API_ID = input("Enter your API_ID (1234567): ")
+  if not (API_ID or len(API_ID) == 7):
+    print("\n\nPlease enter a valid 7 digit API ID.\n\n")
+    continue
+  break
 
-
-
-API_HASH = input("\nEnter API HASH: ")
-
+while True:
+  API_HASH = input("\nEnter API HASH: ")
+  if not API_HASH:
+    print("\n\nPlease enter a valid API HASH.\n\n")
+    continue
+  break
 
 
 # create a new pyrogram session
 with Client("tronbot", api_id=API_ID, api_hash=API_HASH) as app:
-  app.send_message("me", f"This Is Your Tron Userbot • [ `SESSION` ]\n\n```{app.export_session_string()}```\n\n⚠️• Don't share this with anyone !!\n\nCreate session online • [ Press Here](https://replit.com/@beastzx18/Tron-Userbot-Session)", disable_web_page_preview=True) 
-  print("\n\nYour String Session Is Successfully Saved In Telegram Saved (Cloud) Messages !! Don't Share It With Anyone!! Anyone having your session can use (hack) your telegram account !")
-  
+  app.send_message(
+    "me",
+    f"This Is Your Tron Userbot • [ `SESSION` ]\n\n```{app.export_session_string()}```\n\n⚠️• Don't share this with anyone !!\n\nCreate session online • [ Press Here ](https://replit.com/@beastzx18/Tron-Userbot-Session)",
+    disable_web_page_preview=True
+  )
+  print("\n\nYour String Session Is Successfully Saved In Telegram Saved (Cloud) Messages !! Don't Share It With Anyone!! Anyone having your session can use (Hack) your Telegram Account !")
