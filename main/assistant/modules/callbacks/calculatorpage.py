5b0d706d59f75f021e30accb1b14ddca60f62e3f
vs423502@gmail.com <vs423502@gmail.com>
2023-01-22 22:22:32 +0530

    update

--- main/assistant/modules/callbacks/calculatorpage.py
+++ main/assistant/modules/callbacks/calculatorpage.py
@@ -89,6 +89,7 @@ async def calculator_evaluate_callback(_, cb: CallbackQuery):
                 text = "Error !"
                 print(e)
         else:
+            print(message)
             text = message.caption if message.caption else "" + text
 
         await cb.edit_message_text(
