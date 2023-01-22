df536c0c7a2597851e554d25a9a450ad37aaa349
vs423502@gmail.com <vs423502@gmail.com>
2023-01-22 21:23:03 +0530

    update

--- main/assistant/modules/callbacks/calculatorpage.py
+++ main/assistant/modules/callbacks/calculatorpage.py
@@ -1,6 +1,8 @@
 """
 This file creates extra page tab menu in helpdex
 """
+import struct
+import base64
 
 from pyrogram import filters
 from pyrogram.types import CallbackQuery
@@ -9,39 +11,89 @@ from main.userbot.client import app
 
 
 
+reply_markup=app.buildMarkup(
+                [
+                    app.buildButton("1", "cal-(1)"),
+                    app.buildButton("2", "cal-(2)"),
+                    app.buildButton("3", "cal-(3)"),
+                    app.buildButton("-", "cal-(-)")
+                ],
+                [
+                    app.buildButton("4", "cal-(4)"),
+                    app.buildButton("5", "cal-(5)"),
+                    app.buildButton("6", "cal-(6)"),
+                    app.buildButton("+", "cal-(+)")
+                ],
+                [
+                    app.buildButton("7", "cal-(7)"),
+                    app.buildButton("8", "cal-(8)"),
+                    app.buildButton("9", "cal-(9)"),
+                    app.buildButton("×", "cal-(*)")
+                ],
+                [
+                    app.buildButton("0", "cal-(0)"),
+                    app.buildButton("C", "cal-(C)"),
+                    app.buildButton("del", "cal-(D)"),
+                    app.buildButton("÷", "cal-(/)")
+                ],
+                [app.buildButton("=", "cal-(=)")]
+                
+            )
+
 
 @app.bot.on_callback_query(filters.regex("calculator-tab"))
 async def calculator_callback(_, cb: CallbackQuery):
     try:
         await cb.edit_message_text(
             text=" ",
-            reply_markup=app.buildMarkup(
-                [
-                    app.buildButton("1", "one-tab"),
-                    app.buildButton("2", "two-tab"),
-                    app.buildButton("3", "three-tab"),
-                    app.buildButton("-", "subtraction-tab")
-                ],
-                [
-                    app.buildButton("4", "four-tab"),
-                    app.buildButton("5", "five-tab"),
-                    app.buildButton("6", "six-tab"),
-                    app.buildButton("+", "addition-tab")
-                ],
-                [
-                    app.buildButton("7", "seven-tab"),
-                    app.buildButton("8", "eight-tab"),
-                    app.buildButton("9", "nine-tab"),
-                    app.buildButton("×", "multiplication-tab")
-                ],
-                [
-                    app.buildButton("0", "zero-tab"),
-                    app.buildButton("C", "clear-tab"),
-                    app.buildButton("del", "del-tab"),
-                    app.buildButton("=", "equate-tab")
-                ]
-                
+            reply_markup=reply_markup
+        )
+    except Exception as e:
+        await app.error(e)
+
+
+
+
+@app.bot.on_callback_query(filters.regex(r"."))
+async def calculator_evaluate_callback(_, cb: CallbackQuery):
+    try:
+        if cb.inline_message_id:
+            dc_id, message_id, chat_id, query_id = struct.unpack(
+                "<iiiq",
+                base64.urlsafe_b64decode(
+                    cb.inline_message_id + '=' * (
+                        len(cb.inline_message_id) % 4
+                    )
+                )
             )
+
+            message = await app.get_messages(
+                chat_id=int(str(-100) + str(chat_id)[1:]),
+                message_ids=message_id
+            )
+        elif cb.message:
+                message = cb.message
+        else:
+            return
+
+        text = cb.data[-2]
+
+        if text=="C":
+            text = ""
+        elif text=="D":
+            text = text[:-1] if text else ""
+        elif text=="=":
+            try:
+                text = eval(text) if text else ""
+            except Exception as e:
+                text = "Error !"
+                print(e)
+        else:
+            text = message.caption + text
+
+        await cb.edit_message_text(
+            text=" ",
+            reply_markup=reply_markup
         )
     except Exception as e:
         await app.error(e)
