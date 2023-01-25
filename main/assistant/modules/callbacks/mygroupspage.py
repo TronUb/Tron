8736fa45052d64d7fa79caf60a379906e1025949
vs423502@gmail.com <vs423502@gmail.com>
2023-01-26 00:37:49 +0530

    updates

--- main/assistant/modules/callbacks/mygroupspage.py
+++ main/assistant/modules/callbacks/mygroupspage.py
@@ -14,11 +14,19 @@ from main.userbot.client import app
 @app.alert_user
 async def mygroups_callback(_, cb: CallbackQuery):
     try:
+        await cb.edit_message_text(
+            text="<i>Fetching Your Groups . . .,</i>"
+        )
         buttons = [
             [app.buildButton(x.chat.title, str(x.chat.id))] async for x in 
             app.get_dialogs() if x.chat.type == enums.ChatType.SUPERGROUP and 
             x.chat.is_creator
-        ]
+        ] + [
+                [
+                    app.buildButton("Home", "close-tab"),
+                    app.buildButton("Back", "home-tab")
+                ]
+            ]
         await cb.edit_message_text(
             text="Available Groups That Belong To You.",
             reply_markup=app.buildMarkup(*buttons)
