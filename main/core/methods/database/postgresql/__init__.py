b3fe9b20371013dd6da5be14564a8a5a2f110995
Vikram Vishwakarma <vs423502@gmail.com>
2024-08-22 10:41:06 +0530

    solving database locks

--- main/core/methods/database/postgresql/__init__.py
+++ main/core/methods/database/postgresql/__init__.py
@@ -1,9 +1,8 @@
 import sys
-
+import atexit
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
-
 
 try:
     from main import Config
@@ -15,14 +14,26 @@ database = "sqlite:///tron.db"
 if Config is not None:
     database = getattr(Config, "DB_URI", database)
 
-
 def start() -> scoped_session:
     engine = create_engine(database)
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
 
-
 BASE = declarative_base()
 SESSION = start()
 
+def close_session():
+    """Close the session to avoid database locks."""
+    SESSION.remove()
+
+atexit.register(close_session)
+
+import signal
+
+def handle_signal(signal_number, frame):
+    close_session()
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, handle_signal)
+signal.signal(signal.SIGTERM, handle_signal)
